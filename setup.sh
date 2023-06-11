6b59a7525fc9a5ab197ec95b35c35e3cf76efa71
Pinky Gautam <pkgtm1998@gmail.com>
2023-06-11 20:58:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,15 @@ pip3 install -r requirements.txt
 
 # initialize oltp and olap databases
 python3 setup_dbs.py
+
+# shutdown existing brokers (if any)
+docker-compose down
+
+# start kafka broker
+docker-compose up -d
+
+# wait for broker to start
+sleep 12
+
+# create topics to allow parallelization of producer and consumer w/o topic not exist error
+python3 kafka/admin.py
