18999c037ff86c3d53ce37d3348d1c137dd77ce6
Pinky Gautam <pkgtm1998@gmail.com>
2023-06-11 21:00:13 +0700

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,23 +1,8 @@
 # activate virtual env
 source venv/bin/activate
 
-# shutdown existing brokers (if any)
-docker-compose down
-
-# start kafka broker
-docker-compose up -d
-
 # create logs folder if not exist
 mkdir -p logs/
 
-# wait for broker to start
-sleep 12
-
-# create topics to allow parallelization of producer and consumer w/o topic not exist error
-python3 kafka/admin.py
-
-# stream data to kafka and consume
+# stream oltp data to kafka and consume i.e stage data in olap database
 (python3 kafka/producer.py > logs/producer.txt) & python3 kafka/consumer.py
-
-# shutdown brokers
-docker-compose down
