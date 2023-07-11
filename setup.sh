4ee3f3a55e47e7f5fd0e74ad416b7432304c7d32
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-12 00:02:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,18 +5,11 @@ source venv/bin/activate
 # install required dependencies
 pip3 install -r requirements.txt
 
-# initialize oltp and olap databases
-python3 setup_dbs.py
-
-# shutdown existing brokers (if any)
-docker-compose down
-
-# start kafka broker
+# start containers
 docker-compose up -d
 
-# insert generated data to oltp database
-python3 seed_oltp.py && \
-# wait for services to start properly 
-sleep 60 && \
-# register mysql database to kafka connect
-python3 kafka_connect/register_mysql.py
+# wait for services to start properly
+sleep 60
+
+# initialize oltp and olap databases
+python3 setup_dbs.py
