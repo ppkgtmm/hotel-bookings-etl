d92a061fc657ceb3e20ebb86c7f36653a0f56eb8
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-19 22:30:04 +0700

    Update booking.py

--- generators/booking.py
+++ generators/booking.py
@@ -4,12 +4,20 @@ import pandas as pd
 from datetime import date, timedelta
 from room import weights, room_types, floors
 import math
+from dotenv import load_dotenv
+from os import getenv
 
-data_dir = "data/"
-users_file = "data/person/users.csv"
-guests_file = "data/person/guests.csv"
+load_dotenv()
+
+data_dir = getenv("SEED_DIR")
+seed = getenv("SEED")
+
+users_file = data_dir + "users.csv"
+guests_file = data_dir + "guests.csv"
 
 fake = Faker()
+Faker.seed(seed)
+
 users = pd.read_csv(users_file)
 guests = pd.read_csv(guests_file)
 user_count = users.shape[0]
@@ -33,14 +41,16 @@ def generate_room_preference(types_counts: list, room_counts: list):
 
 
 def generate_bookings(
-    count: int, max_stay: int, max_types: int, max_count_per_type: int
+    count: int,
+    max_stay: int,
+    # max_types: int, max_count_per_type: int
 ):
     stay_duration = list(range(1, max_stay + 1))
     max_stay, sum_stay = max(stay_duration), sum(stay_duration)
     stay_weight = [(max_stay - sd + 1) / sum_stay for sd in stay_duration]
 
-    types = list(range(1, max_types + 1))
-    room_counts = list(range(1, max_count_per_type + 1))
+    # types = list(range(1, max_types + 1))
+    # room_counts = list(range(1, max_count_per_type + 1))
 
     bookings = pd.DataFrame(
         [],
@@ -58,42 +68,42 @@ def generate_bookings(
             end_date=checkin + timedelta(days=-1),
         )
         user = users.loc[_ % user_count, "email"]
-        if bookings.shape[0] == 0:
-            guest = random.choice(guests.email)
-        else:
-            if random.random() >= 0.65:
-                guest = random.choice(guests.email)
-            else:
-                guest = user
-            overlap_count = bookings[
-                (bookings["guest"] == guest)
-                & (
-                    (
-                        (bookings["checkin"] <= checkin)
-                        & (checkin <= bookings["checkout"])
-                    )
-                    | (
-                        (bookings["checkin"] <= checkout)
-                        & (checkout <= bookings["checkout"])
-                    )
-                    | (
-                        (checkin <= bookings["checkin"])
-                        & (checkout >= bookings["checkout"])
-                    )
-                )
-            ].shape[0]
-            if overlap_count > 0:
-                continue
-        rooms_preference = generate_room_preference(types, room_counts)
+        # if bookings.shape[0] == 0:
+        #     guest = random.choice(guests.email)
+        # else:
+        #     if random.random() >= 0.65:
+        #         guest = random.choice(guests.email)
+        #     else:
+        #         guest = user
+        #     overlap_count = bookings[
+        #         (bookings["guest"] == guest)
+        #         & (
+        #             (
+        #                 (bookings["checkin"] <= checkin)
+        #                 & (checkin <= bookings["checkout"])
+        #             )
+        #             | (
+        #                 (bookings["checkin"] <= checkout)
+        #                 & (checkout <= bookings["checkout"])
+        #             )
+        #             | (
+        #                 (checkin <= bookings["checkin"])
+        #                 & (checkout >= bookings["checkout"])
+        #             )
+        #         )
+        #     ].shape[0]
+        #     if overlap_count > 0:
+        #         continue
+        # rooms_preference = generate_room_preference(types, room_counts)
         booking = pd.DataFrame(
             [
                 dict(
                     user=user,
-                    guest=guest,
+                    # guest=guest,
                     checkin=checkin,
                     checkout=checkout,
                     payment=payment,
-                    rooms_preference=rooms_preference,
+                    # rooms_preference=rooms_preference,
                 )
             ]
         )
@@ -102,30 +112,34 @@ def generate_bookings(
 
 
 if __name__ == "__main__":
-    num_bookings = 85
-    max_stay = 20
-    max_types = 3
-    max_count_per_type = 2
+    num_bookings = 90
+    max_stay = 15
+    # max_types = 3
+    # max_count_per_type = 2
 
-    bookings = generate_bookings(num_bookings, max_stay, max_types, max_count_per_type)
+    bookings = generate_bookings(
+        num_bookings,
+        max_stay,
+        # max_types, max_count_per_type
+    )
 
-    sort = bookings.sort_values(["guest", "checkin"])
-    shifted = sort.join(sort[["guest", "checkout"]].shift(), rsuffix="_prev")
-    assert shifted.shape[0] == bookings.shape[0]
-    assert (
-        shifted[
-            (shifted["guest"] == shifted["guest_prev"])
-            & (
-                shifted["checkout_prev"].between(
-                    shifted["checkin"], shifted["checkout"]
-                )
-            )
-        ].shape[0]
-        == 0
-    )
-    bookings = bookings.explode("rooms_preference")
-    bookings[["room_type", "count", "min_floor"]] = bookings["rooms_preference"].apply(
-        pd.Series
-    )
-    bookings.drop(columns=["rooms_preference"], inplace=True)
+    # sort = bookings.sort_values(["guest", "checkin"])
+    # shifted = sort.join(sort[["guest", "checkout"]].shift(), rsuffix="_prev")
+    # assert shifted.shape[0] == bookings.shape[0]
+    # assert (
+    #     shifted[
+    #         (shifted["guest"] == shifted["guest_prev"])
+    #         & (
+    #             shifted["checkout_prev"].between(
+    #                 shifted["checkin"], shifted["checkout"]
+    #             )
+    #         )
+    #     ].shape[0]
+    #     == 0
+    # )
+    # bookings = bookings.explode("rooms_preference")
+    # bookings[["room_type", "count", "min_floor"]] = bookings["rooms_preference"].apply(
+    #     pd.Series
+    # )
+    # bookings.drop(columns=["rooms_preference"], inplace=True)
     bookings.to_csv(data_dir + "bookings.csv", index=False)
