f7478fda33a8db12acb2470296adb1da02e7ea3a
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-21 00:29:45 +0700

    Update seed_oltp.py

--- seed_oltp.py
+++ seed_oltp.py
@@ -4,9 +4,11 @@ from dotenv import load_dotenv
 from os import getenv
 import pandas as pd
 from sqlalchemy import create_engine, text
+from faker.generator import random
 
 load_dotenv()
 
+max_rooms = 5
 
 db_host = getenv("DB_HOST")
 db_user = getenv("DB_USER")
@@ -15,6 +17,10 @@ db_port = getenv("DB_PORT")
 db_name = getenv("OLTP_DB")
 
 data_dir = getenv("SEED_DIR")
+seed = getenv("SEED")
+
+random.seed(seed)
+
 connection_string = (
     f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:3306/{db_name}"
 )
@@ -98,58 +104,62 @@ def load_bookings():
 
 
 def load_booking_rooms():
-    pd.read_csv(data_dir + "bookings.csv").to_sql(
-        "bookings_temp", conn, index=False, if_exists="replace"
-    )
-    conn.execute(text("DELETE FROM booking_rooms;"))
-    conn.execute(text("ALTER TABLE booking_rooms AUTO_INCREMENT = 1;"))
-    room_preference = conn.execute(
-        text(
-            """
-            SELECT b.id booking, b.checkin, b.checkout, bt.room_type, bt.min_floor, FLOOR(SUM(bt.count)) room_cnt
-            FROM bookings_temp bt
-            LEFT JOIN users u
-            ON bt.user = u.email
-            LEFT JOIN bookings b
-            ON b.payment = bt.payment AND b.user = u.id
-            GROUP BY 1, 2, 3, 4, 5
-            ORDER BY 1
-            """
+    room_counts = list(range(1, max_rooms + 1))
+    sum_count = sum(room_counts)
+    count_weight = [(max_rooms - rc + 1) / sum_count for rc in room_counts]
+    bookings = pd.read_sql("SELECT id, checkin, checkout FROM bookings", conn)
+    room_types = [rt[0] for rt in conn.execute(text("SELECT id FROM roomtypes"))]
+    guests = set([g[0] for g in conn.execute(text("SELECT id FROM guests"))])
+    booking_rooms = []
+    for booking in bookings.to_dict(orient="records"):
+        checkin, checkout = booking["checkin"], booking["checkout"]
+        overlapping = pd.read_sql(
+            f"""
+            SELECT guest, room, type
+            FROM booking_rooms br
+            INNER JOIN bookings b
+            ON b.id = br.booking
+            INNER JOIN rooms r
+            ON br.room = r.id
+            WHERE (b.checkin <= {checkin} AND {checkin} <= b.checkout)
+            OR (b.checkin <= {checkout} AND {checkout} <= b.checkout)
+            OR ({checkin} <= b.checkin AND {checkout} >= b.checkout)
+            """,
+            conn,
         )
-    )
-    for row in room_preference:
-        rooms = conn.execute(
-            text(
-                f"""
-                SELECT DISTINCT {row.booking} booking, r.id room
-                FROM rooms r
-                    INNER JOIN (
-                        SELECT id
-                        FROM roomtypes
-                        WHERE name = '{row.room_type}'
-                    ) rt
-                ON r.type = rt.id AND r.floor >= {row.min_floor}
-                LEFT JOIN booking_rooms br
-                ON br.room = r.id
-                LEFT JOIN bookings eb
-                ON br.booking = eb.id AND (
-                (DATE('{row.checkin}') BETWEEN eb.checkin AND eb.checkout)
-                OR (DATE('{row.checkout}') BETWEEN eb.checkin AND eb.checkout)
-                OR (DATE('{row.checkin}') <= eb.checkin AND DATE('{row.checkout}') >= eb.checkout)
-                )
-                WHERE eb.id IS NULL
-                ORDER BY 1
-                LIMIT {row.room_cnt};
-            """
-            )
+        num_rooms = random.choices(room_counts, weights=count_weight, k=1)[0]
+        guest, avail_guests = [], guests - set(overlapping.guest.to_list())
+        for _ in range(num_rooms):
+            avail_guests = avail_guests - set(guest)
+            assert len(avail_guests) >= 1
+            # print("not enough guests available for booking", booking["id"])
+            # continue
+            guest.append(random.choice(list(avail_guests)))
+        room_type = random.choices(room_types, k=num_rooms)
+        room_query = f"SELECT id FROM rooms WHERE type IN ({', '.join([str(rt) for rt in room_type])})"
+        rooms = set([r[0] for r in conn.execute(text(room_query))])
+        assigned_rooms = []
+        for type in room_type:
+            booked_rooms = overlapping[overlapping.type == type].room.to_list()
+            avail_rooms = list(rooms - set(booked_rooms) - set(assigned_rooms))
+            assert len(avail_rooms) >= 1
+            # if avail_rooms == []:
+            #     print("no rooms available")
+            #     continue
+            assigned_rooms.append(random.choice(avail_rooms))
+        assigned_rooms, guest = list(set(assigned_rooms)), list(set(guest))
+        num_deduped = min(len(assigned_rooms), len(guest))
+        booking_rooms.append(
+            {
+                "booking": booking["id"],
+                "room": assigned_rooms[0:num_deduped],
+                "guest": guest[0:num_deduped],
+            }
         )
-        for room in rooms:
-            conn.execute(
-                text(
-                    f"INSERT INTO booking_rooms (booking, room) VALUES ({room.booking}, {room.room})"
-                )
-            )
-    conn.execute(text("DROP TABLE bookings_temp;"))
+    booking_rooms = pd.DataFrame(booking_rooms)
+    booking_rooms = booking_rooms.explode(["room", "guest"])
+    booking_rooms.to_sql("booking_rooms", conn, index=False, if_exists="append")
+    conn.commit()
 
 
 def load_booking_room_addons():
@@ -210,7 +220,7 @@ if __name__ == "__main__":
     load_person()
     load_rooms()
     load_bookings()
-    # load_booking_rooms()
+    load_booking_rooms()
     # load_booking_room_addons()
     conn.close()
     engine.dispose()
