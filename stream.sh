d53073223df142cce99d8372d84b5501542d9e20
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-25 22:28:01 +0700

    Delete stream.sh

--- stream.sh
+++
@@ -1,8 +0,0 @@
-# activate virtual env
-source venv/bin/activate
-
-# create logs folder if not exist
-mkdir -p logs/
-
-# stream oltp data to kafka and consume i.e stage data in olap database
-(python3 kafka/producer.py > logs/producer.txt) & python3 kafka/consumer.py
