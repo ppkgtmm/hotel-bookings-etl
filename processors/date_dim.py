1bcf573b829577868458016685fe0cfe54e4ef22
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-28 02:17:17 +0700

    Update date_dim.py

--- processors/date_dim.py
+++ processors/date_dim.py
@@ -1,21 +1,27 @@
 from datetime import timedelta, datetime
-import pandas as pd
 from helpers import Processor
+import math
+from sqlalchemy import text
 
-df_columns = ["id", "datetime", "date", "month", "quarter", "year"]
-start_date = pd.to_datetime("2021-01-01") + timedelta(days=-90)
+start_date = datetime.strptime("2021-01-01 00:00:00", "%Y-%m-%d %H:%M:%S") + timedelta(
+    days=-90
+)
 curr_date = start_date
 max_date = datetime.now()
 data = []
 while curr_date <= max_date:
     data.append(
-        (
-            curr_date.strftime("%Y%m%d%H%M%S"),
-            curr_date,
-            curr_date.date(),
-            curr_date.strftime("%Y-%m-01"),
-            datetime(year=curr_date.year, month=(curr_date.quarter - 1) * 3 + 1, day=1),
-            curr_date.year,
+        dict(
+            id=int(curr_date.strftime("%Y%m%d%H%M%S")),
+            datetime=curr_date,
+            date=curr_date.date(),
+            month=curr_date.strftime("%Y-%m-01"),
+            quarter=datetime(
+                year=curr_date.year,
+                month=(math.ceil(curr_date.month / 3) - 1) * 3 + 1,
+                day=1,
+            ).date(),
+            year=curr_date.year,
         )
     )
     curr_date += timedelta(minutes=30)
@@ -23,6 +29,11 @@ while curr_date <= max_date:
 Processor.load_envars()
 Processor.setup_db_conn()
 
-pd.DataFrame(data, columns=df_columns).to_sql("dim_date", Processor.conn)
-
+Processor.conn.execute(
+    text(
+        "INSERT INTO dim_date VALUES (:id, :datetime, :date, DATE(:month), :quarter, :year)"
+    ),
+    data,
+)
+Processor.conn.commit()
 Processor.teardown_db_conn()
