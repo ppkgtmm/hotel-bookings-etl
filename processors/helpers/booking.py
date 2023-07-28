c464a27a56e95a6e344660118177ee1e40c1f753
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-28 19:56:52 +0700

    Create booking.py

---
+++ processors/helpers/booking.py
@@ -0,0 +1,19 @@
+import json
+from helpers.processor import Processor
+
+
+class BookingProcessor(Processor):
+    columns = ["id", "checkin", "checkout"]
+
+    def __init__(self):
+        super().__init__()
+
+    def process(self, row):
+        if row.topic != "oltp_hotel.oltp_hotel.bookings":
+            return
+        payload = json.loads(row.value)["payload"]["after"]
+        if not payload:
+            return
+        payload["checkin"] = super().to_date(payload["checkin"])
+        payload["checkout"] = super().to_date(payload["checkout"])
+        super().upsert_to_db("stg_booking", payload, BookingProcessor.columns)
