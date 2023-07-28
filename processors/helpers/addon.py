7f80cbe55337a239e7550bd386dd0ca7f9dcc7b7
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-28 19:22:11 +0700

    update processors

--- processors/helpers/addon.py
+++ processors/helpers/addon.py
@@ -3,7 +3,7 @@ from helpers.processor import Processor
 
 
 class AddonProcessor(Processor):
-    columns = ["_id", "name", "price"]
+    columns = ["_id", "name", "price", "created_at"]
 
     def __init__(self):
         super().__init__()
@@ -14,4 +14,5 @@ class AddonProcessor(Processor):
         payload = json.loads(row.value)["payload"]["after"]
         if not payload:
             return
-        super().insert_to_db("dim_addon", payload)
+        payload["created_at"] = super().to_datetime(payload["created_at"])
+        super().insert_to_db("dim_addon", payload, AddonProcessor.columns)
--- processors/helpers/room_type.py
+++ processors/helpers/room_type.py
@@ -3,7 +3,7 @@ from helpers.processor import Processor
 
 
 class RoomTypeProcessor(Processor):
-    columns = ["_id", "name", "price"]
+    columns = ["_id", "name", "price", "created_at"]
 
     def __init__(self):
         super().__init__()
@@ -14,4 +14,5 @@ class RoomTypeProcessor(Processor):
         payload = json.loads(row.value)["payload"]["after"]
         if not payload:
             return
-        super().insert_to_db("dim_roomtype", payload)
+        payload["created_at"] = super().to_datetime(payload["created_at"])
+        super().insert_to_db("dim_roomtype", payload, RoomTypeProcessor.columns)
