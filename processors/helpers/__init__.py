5ab83f433dcae04c6ac31f902791af65d664d594
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-28 19:56:57 +0700

    Update __init__.py

--- processors/helpers/__init__.py
+++ processors/helpers/__init__.py
@@ -4,3 +4,4 @@ from helpers.addon import AddonProcessor
 from helpers.room_type import RoomTypeProcessor
 from helpers.processor import Processor
 from helpers.room import RoomProcessor
+from helpers.booking import BookingProcessor
