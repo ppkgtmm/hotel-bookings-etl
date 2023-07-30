45603f48fbf8be2c7e8f19c1c7ad9842796506db
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-30 20:12:09 +0700

    Update run.sh

--- generators/run.sh
+++ generators/run.sh
@@ -1,5 +1,15 @@
-python3 location.py
-python3 static.py
-python3 person.py
-python3 room.py
-python3 booking.py
+echo "downloading location data"
+python3 generators/location.py
+echo "downloaded location data"
+echo "generating room types & addons data"
+python3 generators/static.py
+echo "generated room types & addons data"
+echo "generating users & guests data"
+python3 generators/person.py
+echo "generated users & guests data"
+echo "generating rooms data"
+python3 generators/room.py
+echo "generated rooms data"
+echo "generating hotel bookings data"
+python3 generators/booking.py
+echo "generated hotel bookings data"
