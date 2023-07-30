6acadbca10712041184ad6558027e8580e07e31e
Pinky Gautam <pkgtm1998@gmail.com>
2023-07-30 14:20:09 +0700

    Update register_mysql.py

--- kafka_connect/register_mysql.py
+++ kafka_connect/register_mysql.py
@@ -31,7 +31,10 @@ def get_config(**kwargs):
     config["config"][
         "schema.history.internal.kafka.topic"
     ] = f"schema-changes.{kwargs.get('DB_NAME')}"
-
+    config["config"][
+        "transforms.Reroute.topic.regex"
+    ] = f'(.*){kwargs.get("DB_NAME")}(.*)'
+    config["config"]["transforms.Reroute.topic.replacement"] = "$1data"
     return config
 
 
