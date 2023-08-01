bf0520a2f6ea1ceb262502e92e90f78b2aa0007b
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-01 13:51:39 +0700

    Update guest.py

--- processors/helpers/guest.py
+++ processors/helpers/guest.py
@@ -4,16 +4,19 @@ from helpers.helper import ProcessingHelper
 
 class GuestProcessor(ProcessingHelper):
     columns = ["id", "email", "dob", "gender"]
-    stg_columns = columns + ["location"]
+    stg_columns = columns + ["location", "updated_at"]
 
     def __init__(self):
         super().__init__()
 
     def process(self, row):
-        payload = json.loads(row.value)["payload"]
+        payload = json.loads(row.value).get("payload", {})
         payload = payload.get("after")
         if not payload:
             return
         payload["dob"] = ProcessingHelper.to_date(payload["dob"])
-        ProcessingHelper.upsert_to_db("stg_guest", payload, GuestProcessor.stg_columns)
+        payload["updated_at"] = ProcessingHelper.to_datetime(payload["updated_at"])
+        ProcessingHelper.insert_to_db(
+            "stg_guest", payload, GuestProcessor.stg_columns, prepare=False
+        )
         ProcessingHelper.upsert_to_db("dim_guest", payload, GuestProcessor.columns)
