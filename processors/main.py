2db05af61f67ca0b46c1719147cec830ddcae1c8
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-01 23:59:23 +0700

    Update main.py

--- processors/main.py
+++ processors/main.py
@@ -1,36 +1,25 @@
 from pyspark.sql import SparkSession
-
-from pyspark.sql.functions import (
-    from_json,
-    col,
-    timestamp_seconds,
-    expr,
-)
-from pyspark.sql.types import (
-    IntegerType,
-    LongType,
-    FloatType,
-    StringType,
-    StructField,
-    StructType,
-    MapType,
-)
 from dotenv import load_dotenv
 from os import getenv
-from helpers import (
-    AddonProcessor,
-    BookingProcessor,
-    BookingAddonProcessor,
-    BookingRoomProcessor,
-    RoomProcessor,
-    RoomTypeProcessor,
-    GuestProcessor,
-    LocationProcessor,
+from helper import (
+    process_addons,
+    process_roomtypes,
+    process_locations,
+    process_rooms,
+    process_guests,
+    process_bookings,
+    process_booking_rooms,
+    process_booking_addons,
+    engine,
+    conn,
 )
+import traceback
+from delta import configure_spark_with_delta_pip
 
 load_dotenv()
 
 MAX_OFFSETS = 10
+
 OLTP_DB = getenv("OLTP_DB")
 BROKER = getenv("KAFKA_BOOTSTRAP_SERVERS_INTERNAL")
 # BROKER = getenv("KAFKA_BOOTSTRAP_SERVERS")
@@ -44,85 +33,20 @@ BOOKINGS_TABLE = getenv("BOOKINGS_TABLE")
 BOOKING_ROOMS_TABLE = getenv("BOOKING_ROOMS_TABLE")
 BOOKING_ADDONS_TABLE = getenv("BOOKING_ADDONS_TABLE")
 
-
-br_schema = StructType(
-    [
-        StructField("id", IntegerType()),
-        StructField("booking", IntegerType()),
-        StructField("room", IntegerType()),
-        StructField("guest", IntegerType()),
-        StructField("created_at", LongType()),
-        StructField("updated_at", LongType()),
-    ]
-)
-
-ba_schema = StructType(
-    [
-        StructField("id", IntegerType()),
-        StructField("booking_room", IntegerType()),
-        StructField("addon", IntegerType()),
-        StructField("quantity", IntegerType()),
-        StructField("datetime", LongType()),
-        StructField("created_at", LongType()),
-        StructField("updated_at", LongType()),
-    ]
-)
-
-g_schema = StructType(
-    [
-        StructField("id", IntegerType()),
-        StructField("firstname", StringType()),
-        StructField("lastname", StringType()),
-        StructField("gender", StringType()),
-        StructField("email", StringType()),
-        StructField("dob", IntegerType()),
-        StructField("location", IntegerType()),
-        StructField("created_at", LongType()),
-        StructField("updated_at", LongType()),
-    ]
-)
-
-r_schema = StructType(
-    [
-        StructField("id", IntegerType()),
-        StructField("floor", IntegerType()),
-        StructField("number", IntegerType()),
-        StructField("type", IntegerType()),
-        StructField("created_at", LongType()),
-        StructField("updated_at", LongType()),
-    ]
-)
-
-rt_schema = StructType(
-    [
-        StructField("id", IntegerType()),
-        StructField("name", StringType()),
-        StructField("price", FloatType()),
-        StructField("created_at", LongType()),
-        StructField("updated_at", LongType()),
-    ]
-)
-b_schema = StructType(
-    [
-        StructField("id", IntegerType()),
-        StructField("user", IntegerType()),
-        StructField("checkin", IntegerType()),
-        StructField("checkout", IntegerType()),
-        StructField("payment", LongType()),
-        StructField("created_at", LongType()),
-        StructField("updated_at", LongType()),
-    ]
-)
+EXTRA_PACKAGES = [
+    "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1"
+]  # cr. https://stackoverflow.com/questions/54285151/kafka-structured-streaming-kafkasourceprovider-could-not-be-instantiated
 if __name__ == "__main__":
-    spark = (
-        SparkSession.builder.master("local")
-        .appName("hotel oltp processor")
+    builder = (
+        SparkSession.builder.appName("hotel oltp processor")
         .config("spark.driver.memory", "1g")
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
         .config(
-            "spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1"
-        )  # cr. https://stackoverflow.com/questions/54285151/kafka-structured-streaming-kafkasourceprovider-could-not-be-instantiated
-        .getOrCreate()
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        )
     )
+    spark = configure_spark_with_delta_pip(builder, EXTRA_PACKAGES).getOrCreate()
 
     location = (
         spark.readStream.format("kafka")
@@ -133,134 +57,98 @@ if __name__ == "__main__":
         .load()
     )
 
-    location.writeStream.foreach(LocationProcessor()).start()
+    location.writeStream.option(
+        "checkpointLocation", "/tmp/mysql/location/_checkpoints/"
+    ).foreachBatch(process_locations).start()
 
-    # guests = (
-    #     spark.readStream.format("kafka")
-    #     .option("kafka.bootstrap.servers", BROKER)
-    #     .option("subscribe", GUESTS_TABLE)
-    #     .option("startingOffsets", "earliest")
-    #     .load()
-    # )
+    addons = (
+        spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", BROKER)
+        .option("subscribe", ADDONS_TABLE)
+        .option("startingOffsets", "earliest")
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
+        .load()
+    )
 
-    # guests.writeStream.foreach(GuestProcessor()).start()
+    addons.writeStream.option(
+        "checkpointLocation", "/tmp/mysql/addons/_checkpoints/"
+    ).foreachBatch(process_addons).start()
 
-    # addons = (
-    #     spark.readStream.format("kafka")
-    #     .option("kafka.bootstrap.servers", BROKER)
-    #     .option("subscribe", ADDONS_TABLE)
-    #     .option("startingOffsets", "earliest")
-    #     .load()
-    # )
+    roomtypes = (
+        spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", BROKER)
+        .option("subscribe", ROOMTYPES_TABLE)
+        .option("startingOffsets", "earliest")
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
+        .load()
+    )
 
-    # addons.writeStream.foreach(AddonProcessor()).start()
-
-    # roomtypes = (
-    #     spark.readStream.format("kafka")
-    #     .option("kafka.bootstrap.servers", BROKER)
-    #     .option("subscribe", ROOMTYPES_TABLE)
-    #     .option("startingOffsets", "earliest")
-    #     .load()
-    # )
-    # roomtypes.writeStream.foreach(RoomTypeProcessor()).start()
+    roomtypes.writeStream.option(
+        "checkpointLocation", "/tmp/mysql/roomtypes/_checkpoints/"
+    ).foreachBatch(process_roomtypes).start()
 
     rooms = (
         spark.readStream.format("kafka")
         .option("kafka.bootstrap.servers", BROKER)
         .option("subscribe", ROOMS_TABLE)
         .option("startingOffsets", "earliest")
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
         .load()
-        .withColumn(
-            "message",
-            from_json(col("value").cast("string"), MapType(StringType(), StringType())),
-        )
-        .withColumn(
-            "payload",
-            from_json(col("message.payload"), MapType(StringType(), StringType())),
-        )
-        .withColumn("data", from_json(col("payload.after"), r_schema))
-        .withColumn("time_s", col("data.updated_at") / 1000)
-        .select("data", timestamp_seconds("time_s").alias("time"))
     )
 
-    # rooms.writeStream.foreach(RoomProcessor()).start()
+    rooms.writeStream.option(
+        "checkpointLocation", "/tmp/delta/rooms/_checkpoints/"
+    ).foreachBatch(process_rooms).start()
+
+    guests = (
+        spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", BROKER)
+        .option("subscribe", GUESTS_TABLE)
+        .option("startingOffsets", "earliest")
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
+        .load()
+    )
+
+    guests.writeStream.option(
+        "checkpointLocation", "/tmp/delta/guests/_checkpoints/"
+    ).foreachBatch(process_guests).start()
 
     bookings = (
         spark.readStream.format("kafka")
         .option("kafka.bootstrap.servers", BROKER)
         .option("subscribePattern", BOOKINGS_TABLE)
         .option("startingOffsets", "earliest")
-        .option("maxOffsetsPerTrigger", 10)
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
         .load()
-        .withColumn(
-            "message",
-            from_json(col("value").cast("string"), MapType(StringType(), StringType())),
-        )
-        .withColumn(
-            "payload",
-            from_json(col("message.payload"), MapType(StringType(), StringType())),
-        )
-        .withColumn("data", from_json(col("payload.after"), b_schema))
-        .withColumn("time_s", col("data.updated_at") / 1000)
-        .select("data", timestamp_seconds("time_s").alias("time"))
     )
-    # bookings.writeStream.foreach(BookingProcessor()).start()
+
+    bookings.writeStream.foreachBatch(process_bookings).start()
 
     booking_rooms = (
         spark.readStream.format("kafka")
         .option("kafka.bootstrap.servers", BROKER)
         .option("subscribe", BOOKING_ROOMS_TABLE)
         .option("startingOffsets", "earliest")
-        .option("maxOffsetsPerTrigger", 10)
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
         .load()
-        .withColumn(
-            "message",
-            from_json(col("value").cast("string"), MapType(StringType(), StringType())),
-        )
-        .withColumn(
-            "payload",
-            from_json(col("message.payload"), MapType(StringType(), StringType())),
-        )
-        .withColumn("data", from_json(col("payload.after"), br_schema))
-        .withColumn("time_s", col("data.updated_at") / 1000)
-        .select("data", timestamp_seconds("time_s").alias("time"))
     )
 
-    booking_rooms.alias("br").withWatermark("time", "60 seconds").join(
-        bookings.alias("b"),
-        expr(
-            """
-            b.data.id = br.data.booking AND
-            br.time BETWEEN b.time AND b.time + interval 15 seconds
-            """
-        ),
-    ).join(
-        rooms.alias("r"),
-        expr(
-            """
-            r.data.id = br.data.room AND
-            b.time BETWEEN r.time AND r.time + interval 15 seconds
-            """
-        ),
-    ).select(
-        "br.data", "b.data.checkin", "b.data.checkout", "r.data.type"
-    ).writeStream.format(
-        "console"
-    ).outputMode(
-        "append"
-    ).option(
-        "truncate", "false"
-    ).start().awaitTermination()
+    booking_rooms.writeStream.foreachBatch(process_booking_rooms).start()
 
-    # booking_rooms.writeStream.foreach(BookingRoomProcessor()).start()
+    booking_addons = (
+        spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", BROKER)
+        .option("subscribe", BOOKING_ADDONS_TABLE)
+        .option("startingOffsets", "earliest")
+        .option("maxOffsetsPerTrigger", MAX_OFFSETS)
+        .load()
+    )
+    booking_addons.writeStream.foreachBatch(process_booking_addons).start()
 
-    # booking_addons = (
-    #     spark.readStream.format("kafka")
-    #     .option("kafka.bootstrap.servers", BROKER)
-    #     .option("subscribe", BOOKING_ADDONS_TABLE)
-    #     .option("startingOffsets", "earliest")
-    #     .load()
-    # )
-    # booking_addons.writeStream.foreach(BookingAddonProcessor()).start()
-
-    # spark.streams.awaitAnyTermination()
+    try:
+        spark.streams.awaitAnyTermination()
+    except Exception as e:
+        traceback.print_exc()
+    finally:
+        conn.close()
+        engine.dispose()
