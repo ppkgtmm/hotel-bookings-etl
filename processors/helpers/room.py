311abdb520d9fc50f1c14a414edb182afbdf6290
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-01 13:58:07 +0700

    Update room.py

--- processors/helpers/room.py
+++ processors/helpers/room.py
@@ -3,14 +3,15 @@ from helpers.helper import ProcessingHelper
 
 
 class RoomProcessor(ProcessingHelper):
-    columns = ["id", "type"]
+    columns = ["id", "type", "updated_at"]
 
     def __init__(self):
         super().__init__()
 
     def process(self, row):
-        payload = json.loads(row.value)["payload"]
+        payload = json.loads(row.value).get("payload", {})
         payload = payload.get("after")
         if not payload:
             return
+        payload["updated_at"] = ProcessingHelper.to_datetime(payload["updated_at"])
         ProcessingHelper.upsert_to_db("stg_room", payload, RoomProcessor.columns)
