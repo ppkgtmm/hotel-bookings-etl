96a0618dc81a40cbe159d9f9e57e0c7585b6ba69
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-05 19:01:35 +0700

    Update run.sh

--- processors/run.sh
+++ processors/run.sh
@@ -3,9 +3,6 @@
 sleep 30
 
 python3 date_dim.py
-
-wget --directory-prefix=/ https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-j-8.0.33.tar.gz
-
-tar -xf /mysql-connector-j-8.0.33.tar.gz -C /
+# python3 date_dim.py
 
 python3 main.py
