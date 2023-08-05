c7a9daa1884ccfba19e3a0032d7b456d07951554
Pinky Gautam <pkgtm1998@gmail.com>
2023-08-05 21:05:52 +0700

    Update db_writer.py

--- processors/db_writer.py
+++ processors/db_writer.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 from os import getenv
 from sqlalchemy import create_engine, text, Table, MetaData, update
 from sqlalchemy.dialects.mysql import insert
-from sqlalchemy import create_engine, text, Table, MetaData, update
-from sqlalchemy.dialects.mysql import insert
 from constants import *
 from datetime import timedelta
 
@@ -18,141 +16,152 @@ db_name = getenv("OLAP_DB")
 connection_string = "mysql+mysqlconnector://{}:{}@{}:{}/{}".format(
     db_user, db_password, db_host, db_port, db_name
 )
-engine = create_engine(connection_string)
-conn = engine.connect()
-
-metadata = MetaData()
-
-# staging tables
-Location = Table(stg_location_table, metadata, autoload_with=engine)
-Room = Table(stg_room_table, metadata, autoload_with=engine)
-Guest = Table(stg_guest_table, metadata, autoload_with=engine)
-Booking = Table(stg_booking_table, metadata, autoload_with=engine)
-BookingRoom = Table(stg_booking_room_table, metadata, autoload_with=engine)
-BookingAddon = Table(stg_booking_addon_table, metadata, autoload_with=engine)
 
 
-DimAddon = Table(dim_addon_table, metadata, autoload_with=engine)
-DimRoomType = Table(dim_roomtype_table, metadata, autoload_with=engine)
-DimDate = Table(dim_date_table, metadata, autoload_with=engine)
-DimGuest = Table(dim_guest_table, metadata, autoload_with=engine)
-DimLocation = Table(dim_location_table, metadata, autoload_with=engine)
-FactBooking = Table(fct_booking_table, metadata, autoload_with=engine)
-FactPurchase = Table(fct_purchase_table, metadata, autoload_with=engine)
+class DatabaseWriter:
+    def __init__(self) -> None:
+        self.engine = create_engine(connection_string, pool_recycle=3600)
+        self.conn = self.engine.connect()
 
+        self.metadata = MetaData()
 
-def write_dim_date(rows: list[Dict[str, Any]]):
-    query = insert(DimDate).values(rows)
-    query = query.on_duplicate_key_update(
-        datetime=query.inserted.datetime,
-        date=query.inserted.date,
-        month=query.inserted.month,
-        quarter=query.inserted.quarter,
-        year=query.inserted.year,
-    )
-    conn.execute(query)
-    conn.commit()
+        # staging tables
+        self.Location = Table(
+            stg_location_table, self.metadata, autoload_with=self.engine
+        )
+        self.Room = Table(stg_room_table, self.metadata, autoload_with=self.engine)
+        self.Guest = Table(stg_guest_table, self.metadata, autoload_with=self.engine)
+        self.Booking = Table(
+            stg_booking_table, self.metadata, autoload_with=self.engine
+        )
+        self.BookingRoom = Table(
+            stg_booking_room_table, self.metadata, autoload_with=self.engine
+        )
+        self.BookingAddon = Table(
+            stg_booking_addon_table, self.metadata, autoload_with=self.engine
+        )
 
+        self.DimAddon = Table(dim_addon_table, self.metadata, autoload_with=self.engine)
+        self.DimRoomType = Table(
+            dim_roomtype_table, self.metadata, autoload_with=self.engine
+        )
+        self.DimDate = Table(dim_date_table, self.metadata, autoload_with=self.engine)
+        self.DimGuest = Table(dim_guest_table, self.metadata, autoload_with=self.engine)
+        self.DimLocation = Table(
+            dim_location_table, self.metadata, autoload_with=self.engine
+        )
+        self.FactBooking = Table(
+            fct_booking_table, self.metadata, autoload_with=self.engine
+        )
+        self.FactPurchase = Table(
+            fct_purchase_table, self.metadata, autoload_with=self.engine
+        )
 
-def write_dim_addons(rows: list[Dict[str, Any]]):
-    query = insert(DimAddon).values(rows)
-    conn.execute(query)
-    conn.commit()
+    def write_dim_date(self, rows: list[Dict[str, Any]]):
+        query = insert(self.DimDate).values(rows)
+        query = query.on_duplicate_key_update(
+            datetime=query.inserted.datetime,
+            date=query.inserted.date,
+            month=query.inserted.month,
+            quarter=query.inserted.quarter,
+            year=query.inserted.year,
+        )
+        self.conn.execute(query)
+        self.conn.commit()
 
+    def write_dim_addons(self, rows: list[Dict[str, Any]]):
+        query = insert(self.DimAddon).values(rows)
+        self.conn.execute(query)
+        self.conn.commit()
 
-def write_dim_roomtypes(rows: list[Dict[str, Any]]):
-    query = insert(DimRoomType).values(rows)
-    conn.execute(query)
-    conn.commit()
+    def write_dim_roomtypes(self, rows: list[Dict[str, Any]]):
+        query = insert(self.DimRoomType).values(rows)
+        self.conn.execute(query)
+        self.conn.commit()
 
+    def write_dim_locations(self, rows: list[Dict[str, Any]]):
+        query = insert(self.DimLocation).values(rows)
+        query = query.on_duplicate_key_update(
+            state=query.inserted.state, country=query.inserted.country
+        )
+        self.conn.execute(query)
+        self.conn.commit()
 
-def write_dim_locations(rows: list[Dict[str, Any]]):
-    query = insert(DimLocation).values(rows)
-    query = query.on_duplicate_key_update(
-        state=query.inserted.state, country=query.inserted.country
-    )
-    conn.execute(query)
-    conn.commit()
+    def stage_rooms(self, rows: list[Dict[str, Any]]):
+        query = insert(self.Room).values(rows)
+        self.conn.execute(query)
+        self.conn.commit()
 
+    def stage_guests(self, rows: list[Dict[str, Any]]):
+        query = insert(self.Guest).values(rows)
+        self.conn.execute(query)
+        self.conn.commit()
 
-def stage_rooms(rows: list[Dict[str, Any]]):
-    query = insert(Room).values(rows)
-    conn.execute(query)
-    conn.commit()
+    def write_dim_guests(self, rows: list[Dict[str, Any]]):
+        query = insert(self.DimGuest).values(rows)
+        query = query.on_duplicate_key_update(
+            email=query.inserted.email,
+            dob=query.inserted.dob,
+            gender=query.inserted.gender,
+        )
+        self.conn.execute(query)
+        self.conn.commit()
 
+    def stage_bookings(self, rows: list[Dict[str, Any]]):
+        query = insert(self.Booking).values(rows)
+        query = query.on_duplicate_key_update(
+            checkin=query.inserted.checkin, checkout=query.inserted.checkout
+        )
+        self.conn.execute(query)
+        self.conn.commit()
 
-def stage_guests(rows: list[Dict[str, Any]]):
-    query = insert(Guest).values(rows)
-    conn.execute(query)
-    conn.commit()
+    def stage_booking_rooms(self, rows: list[Dict[str, Any]]):
+        query = insert(self.BookingRoom).values(rows)
+        query = query.on_duplicate_key_update(
+            booking=query.inserted.booking,
+            room=query.inserted.room,
+            guest=query.inserted.guest,
+            updated_at=query.inserted.updated_at,
+        )
+        self.conn.execute(query)
+        self.conn.commit()
 
+    def write_fct_bookings(self):
+        query = bookings_query.format(
+            stg_guest_table=stg_guest_table,
+            stg_room_table=stg_room_table,
+            stg_booking_table=stg_booking_table,
+            stg_booking_room_table=stg_booking_room_table,
+        )
+        for row in self.conn.execute(text(query)):
+            (
+                id,
+                checkin,
+                checkout,
+                guest,
+                guest_location,
+                room_type,
+            ) = row
+            current_date = checkin
+            while current_date <= checkout:
+                query = insert(self.FactBooking).values(
+                    guest=guest,
+                    guest_location=guest_location,
+                    roomtype=room_type,
+                    datetime=int(current_date.strftime("%Y%m%d%H%M%S")),
+                )
+                query = query.on_duplicate_key_update(datetime=query.inserted.datetime)
+                self.conn.execute(query)
+                self.conn.commit()
+                mark_processed = (
+                    update(self.BookingRoom)
+                    .where(self.BookingRoom.c.id == id)
+                    .values(processed=True)
+                )
+                self.conn.execute(mark_processed)
+                self.conn.commit()
+                current_date += timedelta(days=1)
 
-def write_dim_guests(rows: list[Dict[str, Any]]):
-    query = insert(DimGuest).values(rows)
-    query = query.on_duplicate_key_update(
-        email=query.inserted.email, dob=query.inserted.dob, gender=query.inserted.gender
-    )
-    conn.execute(query)
-    conn.commit()
-
-
-def stage_bookings(rows: list[Dict[str, Any]]):
-    query = insert(Booking).values(rows)
-    query = query.on_duplicate_key_update(
-        checkin=query.inserted.checkin, checkout=query.inserted.checkout
-    )
-    conn.execute(query)
-    conn.commit()
-
-
-def stage_booking_rooms(rows: list[Dict[str, Any]]):
-    query = insert(BookingRoom).values(rows)
-    query = query.on_duplicate_key_update(
-        booking=query.inserted.booking,
-        room=query.inserted.room,
-        guest=query.inserted.guest,
-        updated_at=query.inserted.updated_at,
-    )
-    conn.execute(query)
-    conn.commit()
-
-
-def write_fct_bookings():
-    query = bookings_query.format(
-        stg_guest_table=stg_guest_table,
-        stg_room_table=stg_room_table,
-        stg_booking_table=stg_booking_table,
-        stg_booking_room_table=stg_booking_room_table,
-    )
-    for row in conn.execute(text(query)):
-        (
-            id,
-            checkin,
-            checkout,
-            guest,
-            guest_location,
-            room_type,
-        ) = row
-        current_date = checkin
-        while current_date <= checkout:
-            data = {
-                "guest": guest,
-                "guest_location": guest_location,
-                "roomtype": room_type,
-                "datetime": int(current_date.strftime("%Y%m%d%H%M%S")),
-            }
-            query = insert(FactBooking).values(**data)
-            query = query.on_duplicate_key_update(**data)
-            conn.execute(query)
-            conn.commit()
-            mark_processed = (
-                update(BookingRoom).where(BookingRoom.c.id == id).values(processed=True)
-            )
-            conn.execute(mark_processed)
-            conn.commit()
-            current_date += timedelta(days=1)
-
-
-def tear_down():
-    conn.close()
-    engine.dispose()
+    def tear_down(self):
+        self.conn.close()
+        self.engine.dispose()
