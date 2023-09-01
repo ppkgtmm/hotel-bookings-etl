e454f2b45eb6494f267c6ce0eaf252cf0fe8cd20
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-02 00:46:49 +0700

    Update setup.sh

--- processors/airflow/setup.sh
+++ processors/airflow/setup.sh
@@ -1,3 +1,5 @@
 airflow db init
 
 airflow users create --username admin --password ${AIRFLOW_ADMIN_PASSWORD} --firstname First --lastname Last --role Admin --email admin@example.com
+
+airflow connections add "olap_db_conn" --conn-uri "mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST_INTERNAL}:${DB_PORT}/${OLAP_DB}"
