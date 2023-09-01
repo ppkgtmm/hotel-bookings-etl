d87561df39a52d7cccbd629c30ceb359a6c15f2f
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-02 01:38:57 +0700

    Create olap_stg_cleanup.py

---
+++ processors/airflow/olap_stg_cleanup.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+
+default_args = dict(
+    owner="airflow",
+    start_date=datetime(2023, 9, 1),
+    depends_on_past=False,
+    schedule_interval="@daily",
+    catchup=False,
+)
+
+dag = DAG(
+    "olap_stg_cleanup",
+    default_args=default_args,
+    max_active_runs=1,  # no concurrent runs
+)
+
+cleanup_booking_addons = PythonOperator(task_id="cleanup_booking_addons", dag=dag)
+cleanup_booking_rooms = PythonOperator(task_id="cleanup_booking_rooms", dag=dag)
+cleanup_bookings = PythonOperator(task_id="cleanup_bookings", dag=dag)
+cleanup_guests = PythonOperator(task_id="cleanup_guests", dag=dag)
+cleanup_rooms = PythonOperator(task_id="cleanup_rooms", dag=dag)
