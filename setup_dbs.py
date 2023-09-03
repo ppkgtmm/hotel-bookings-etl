9643d5f83774740caa9bd8eb1fa2ca988381da5d
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-03 21:57:23 +0700

    Update setup_dbs.py

--- setup_dbs.py
+++ setup_dbs.py
@@ -8,7 +8,9 @@ db_host = getenv("DB_HOST")
 db_port = getenv("DB_PORT")
 db_user = getenv("DB_USER")
 db_password = getenv("DB_PASSWORD")
-
+dbz_user = getenv("DBZ_USER")
+dbz_password = getenv("DBZ_PASSWORD")
+dbz_previllages = getenv("DBZ_PREVILLAGES")
 
 sql_dir = "./db_setup"
 oltp_sql = f"{sql_dir}/oltp_db.sql"
@@ -34,15 +36,25 @@ if __name__ == "__main__":
 
     print("-" * 30)
     print("setting up oltp database")
+
     for q in get_queries(oltp_sql):
         conn.execute(text(q))
-    print("oltp database set up done")
 
+    print("oltp database set up done")
     print("-" * 30)
     print("setting up olap database")
+
     for q in get_queries(olap_sql):
         conn.execute(text(q))
+
     print("olap database set up done")
+    print("-" * 30)
+    print("setting up debezium user")
+
+    conn.execute(f"CREATE USER '{dbz_user}'@'%' IDENTIFIED BY '{dbz_password}'")
+    conn.execute(f"GRANT {dbz_previllages} ON *.* TO '{dbz_user}'@'%'")
+
+    print("done setting user up debezium")
     print("-" * 30)
 
     conn.close()
