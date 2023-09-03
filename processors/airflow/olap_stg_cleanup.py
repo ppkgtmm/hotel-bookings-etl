255c9acf25167f24c5917ae5eed27112c95792c5
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-03 18:00:44 +0700

    Update olap_stg_cleanup.py

--- processors/airflow/olap_stg_cleanup.py
+++ processors/airflow/olap_stg_cleanup.py
@@ -1,12 +1,9 @@
 from airflow import DAG
 from airflow.operators.mysql_operator import MySqlOperator
-from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 from os import getenv
 from utilities.constants import *
-from utilities.db_writer import DatabaseWriter
 
-db_writer = DatabaseWriter()
 
 mysql_conn_id = getenv("AIRFLOW_OLAP_CONN_ID")
 
@@ -22,33 +19,6 @@ dag = DAG(
     "olap_stg_cleanup",
     default_args=default_args,
     max_active_runs=1,  # no concurrent runs
-)
-
-remove_fct_booking = PythonOperator(
-    python_callable=db_writer.remove_fct_bookings,
-    task_id="remove_fct_booking",
-    dag=dag,
-)
-remove_fct_purchase = PythonOperator(
-    python_callable=db_writer.remove_fct_purchases,
-    task_id="remove_fct_purchase",
-    dag=dag,
-)
-write_fct_booking = PythonOperator(
-    python_callable=db_writer.write_fct_bookings,
-    task_id="write_fct_booking",
-    dag=dag,
-)
-write_fct_purchase = PythonOperator(
-    python_callable=db_writer.write_fct_purchases,
-    task_id="write_fct_purchase",
-    dag=dag,
-)
-
-tear_down_db_writer = PythonOperator(
-    python_callable=db_writer.tear_down,
-    task_id="tear_down_db_writer",
-    dag=dag,
 )
 
 cleanup_stg_booking_addons = create_table = MySqlOperator(
@@ -131,17 +101,5 @@ cleanup_rooms = MySqlOperator(
     dag=dag,
 )
 
-remove_fct_booking >> write_fct_booking
-remove_fct_purchase >> write_fct_purchase
-[write_fct_booking, write_fct_purchase] >> tear_down_db_writer
-
-tear_down_db_writer >> [
-    cleanup_stg_booking_addons,
-    cleanup_del_booking_addons,
-    cleanup_stg_booking_rooms,
-    cleanup_del_booking_rooms,
-    cleanup_guests,
-    cleanup_rooms,
-]
 [cleanup_stg_booking_rooms, cleanup_del_booking_rooms] >> cleanup_stg_bookings
 [cleanup_stg_booking_rooms, cleanup_del_booking_rooms] >> cleanup_del_bookings
