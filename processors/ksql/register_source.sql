1036021beeb6bb477a221a5dc4bd850fa9876d53
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-04 15:33:42 +0700

    Create register_source.sql

---
+++ processors/ksql/register_source.sql
@@ -0,0 +1,19 @@
+DROP CONNECTOR IF EXISTS ${DBZ_CONNECTOR};
+
+CREATE SOURCE CONNECTOR ${DBZ_CONNECTOR} WITH (
+    'connector.class' = 'io.debezium.connector.mysql.MySqlConnector',
+    'tasks.max' = '1', 
+    'database.hostname' = '${DB_HOST_INTERNAL}',
+    'database.port' = '${DB_PORT}',
+    'database.user' = '${DBZ_USER}', 
+    'database.password' = '${DBZ_PASSWORD}',
+    'database.server.id' = 1,
+    'database.allowPublicKeyRetrieval' = true,
+    'topic.prefix' = '${OLTP_DB}',
+    'database.include.list' = '${OLTP_DB}', 
+    'table.include.list' = '${OLTP_DB}.*',
+    'database.history.kafka.bootstrap.servers' = '${KAFKA_BOOTSTRAP_SERVERS_INTERNAL}',
+    'database.history.kafka.topic' = 'history.${OLTP_DB}',
+    'min.row.count.to.stream.results' = 0,
+    'snapshot.mode' = 'schema_only'
+);
