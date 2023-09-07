59c30b34d0fc5bfea269716975b1bd8575cbef75
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-08 00:55:02 +0700

    Update register_source.py

--- kafka_connect/register_source.py
+++ kafka_connect/register_source.py
@@ -11,11 +11,11 @@ DBZ_PASSWORD = os.getenv("DBZ_PASSWORD")
 DB_PORT = os.getenv("DB_PORT")
 OLTP_DB = os.getenv("OLTP_DB")
 DBZ_CONNECTOR = os.getenv("DBZ_CONNECTOR")
-KAFKA_CONNECT_SERVER = os.getenv("KAFKA_CONNECT_SERVER")
+KSQL_SERVER_PORT = os.getenv("KSQL_SERVER_PORT")
 KAFKA_BOOTSTRAP_SERVERS_INTERNAL = os.getenv("KAFKA_BOOTSTRAP_SERVERS_INTERNAL")
 
 if __name__ == "__main__":
-    config_path = f"{os.path.dirname(__file__)}/config.json"
+    config_path = os.path.abspath("ksql/register_source.sql")
     with open(config_path, "r") as fp:
         config = fp.read()
     config = (
@@ -30,12 +30,12 @@ if __name__ == "__main__":
         )
     )
     response = requests.post(
-        f"{KAFKA_CONNECT_SERVER}/connectors/",
+        f"http://localhost:{KSQL_SERVER_PORT}/ksql/",
         headers={
             "Accept": "application/json",
             "Content-Type": "application/json",
         },
-        json=json.loads(config),
+        json={"ksql": config},
     )
     print(response.text)
     assert response.status_code == 201
