acd7bc3279477277d309c7f8925b10ff9bf8a9db
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-10 18:21:50 +0700

    Update delete_oltp_records.py

--- tests/delete_oltp_records.py
+++ tests/delete_oltp_records.py
@@ -1,47 +1,39 @@
-from sqlalchemy import create_engine, Table, MetaData, delete, select
+from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 from common import *
 import pandas as pd
 
 booking_id = 1
+booking_q = f"SELECT * FROM {bookings_table} WHERE id = {booking_id}"
+booking_room_q = f"SELECT * FROM {booking_rooms_table} WHERE booking = {booking_id}"
+del_booking_q = f"DELETE FROM {bookings_table} WHERE id = {booking_id}"
+del_booking_room_q = f"DELETE FROM {booking_rooms_table} WHERE booking = {booking_id}"
+
 oltp_engine = create_engine(oltp_conn_str, poolclass=NullPool)
 oltp_conn = oltp_engine.connect()
-metadata = MetaData()
-Booking = Table(bookings_table, metadata, autoload_with=oltp_engine)
-BookingRoom = Table(booking_rooms_table, metadata, autoload_with=oltp_engine)
-BookingAddon = Table(booking_addons_table, metadata, autoload_with=oltp_engine)
 
-
-booking_q = select(Booking).where(Booking.c.id == booking_id)
-booking = oltp_conn.execute(booking_q).fetchone()._asdict()
-
+booking = dict(oltp_conn.execute(booking_q).fetchone())
 print("writing booking to be deleted")
 pd.DataFrame([booking]).to_csv(deleted_booking, index=False)
 
-booking_room_q = select(BookingRoom).where(BookingRoom.c.booking == booking_id)
-booking_rooms = [br._asdict() for br in oltp_conn.execute(booking_room_q).fetchall()]
-
+booking_rooms = [dict(r) for r in oltp_conn.execute(booking_room_q).fetchall()]
 print("writing booking rooms to be deleted")
 pd.DataFrame(booking_rooms).to_csv(deleted_booking_rooms, index=False)
 
-booking_rooms = [br["id"] for br in booking_rooms]
-booking_addon_q = select(BookingAddon).where(
-    BookingAddon.c.booking_room.in_(booking_rooms)
-)
-booking_addons = [ba._asdict() for ba in oltp_conn.execute(booking_addon_q).fetchall()]
+booking_addons = []
+for booking_room in booking_rooms:
+    id = booking_room["id"]
+    booking_addon_q = f"SELECT * FROM {booking_addons_table} WHERE booking_room = {id}"
+    booking_addons += [dict(r) for r in oltp_conn.execute(booking_addon_q).fetchall()]
+    del_booking_addon_q = (
+        f"DELETE FROM {booking_addons_table} WHERE booking_room = {id}"
+    )
+    oltp_conn.execute(del_booking_addon_q)
 
 print("writing booking addons to be deleted")
 pd.DataFrame(booking_addons).to_csv(deleted_booking_addons, index=False)
 
-del_booking_addon_q = delete(BookingAddon).where(
-    BookingAddon.c.booking_room.in_(booking_rooms)
-)
-oltp_conn.execute(del_booking_addon_q)
-
-del_booking_room_q = delete(BookingRoom).where(BookingRoom.c.booking == booking_id)
 oltp_conn.execute(del_booking_room_q)
-
-del_booking_q = delete(Booking).where(Booking.c.id == booking_id)
 oltp_conn.execute(del_booking_q)
 
 oltp_conn.close()
