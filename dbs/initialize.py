7e312c416a402d2bc5b17f3d1a3b270a20d09712
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-12 01:39:14 +0700

    Update initialize.py

--- dbs/initialize.py
+++ dbs/initialize.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-from os import getenv
-from mysql import connector
+from os import getenv, path
+from sqlalchemy import create_engine, text
+
 
 load_dotenv()
 
@@ -12,56 +13,54 @@ dbz_user = getenv("DBZ_USER")
 dbz_password = getenv("DBZ_PASSWORD")
 dbz_previllages = getenv("DBZ_PREVILLAGES")
 
-# sql_dir = "./db_setup"
-# oltp_sql = f"{sql_dir}/oltp_db.sql"
-# olap_sql = f"{sql_dir}/olap_db.sql"
+oltp_sql = path.join(path.abspath(path.dirname(__file__)), "sql/oltp_db.sql")
+olap_sql = path.join(path.abspath(path.dirname(__file__)), "sql/olap_db.sql")
 
-oltp_sql = "oltp_db.sql"
-olap_sql = "olap_db.sql"
+connection_str = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}"
+debezium_sql = f"""
+    DROP USER IF EXISTS {dbz_user};
+    CREATE USER '{dbz_user}'@'%' IDENTIFIED BY '{dbz_password}';
+    GRANT {dbz_previllages} ON *.* TO '{dbz_user}'@'%';
+"""
 
-connection_kwargs = dict(host=db_host, port=db_port, user=db_user, password=db_password)
 
-
-def get_queries(sql_file: str):
+def read_queries(sql_file: str):
     with open(sql_file, "r") as fp:
-        sql = fp.read()
-    for q in sql.split(";"):
-        if q.strip() == "":
+        queries = fp.read()
+    return queries
+
+
+def execute_queries(queries: str, connection_str: str):
+    engine = create_engine(url=connection_str)
+    connection = engine.connect()
+
+    for query in queries.split(";"):
+        if query.strip() == "":
             continue
-        yield q
+
+        connection.execute(text(query))
+
+    connection.close()
+    engine.dispose()
 
 
 if __name__ == "__main__":
-    cnx = connector.connect(**connection_kwargs)
-    cursor = cnx.cursor()
-    # engine = create_engine(connection_string)
-    # conn = engine.connect()
-
     print("-" * 30)
     print("setting up oltp database")
 
-    # for q in get_queries(oltp_sql):
-    #     conn.execute(q)
+    execute_queries(read_queries(oltp_sql), connection_str)
 
     print("oltp database set up done")
     print("-" * 30)
     print("setting up olap database")
 
-    # for q in get_queries(olap_sql):
-    #     conn.execute(q)
+    execute_queries(read_queries(olap_sql), connection_str)
 
     print("olap database set up done")
     print("-" * 30)
     print("setting up debezium user")
 
-    # conn.execute(f"CREATE USER '{dbz_user}'@'%' IDENTIFIED BY '{dbz_password}'")
-    # conn.execute(f"GRANT {dbz_previllages} ON *.* TO '{dbz_user}'@'%'")
+    execute_queries(debezium_sql, connection_str)
 
     print("done setting up debezium user")
     print("-" * 30)
-
-    # conn.close()
-    # engine.dispose()
-
-    cursor.close()
-    cnx.close()
