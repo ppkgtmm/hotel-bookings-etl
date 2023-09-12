0a2c8506999ba2081774c147ac8f12a8b115e873
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-12 20:58:38 +0700

    Update dim_date.py

--- scripts/dim_date.py
+++ scripts/dim_date.py
@@ -1,11 +1,11 @@
 import sys
 from os.path import abspath
-from datetime import timedelta, datetime
-import math
+from datetime import date, timedelta
 from dotenv import load_dotenv
 from os import getenv
+import pandas as pd
 
-sys.path.append(abspath("./"))
+sys.path.append(abspath("."))
 from utilities.db_writer import DatabaseWriter
 
 load_dotenv()
@@ -16,29 +16,16 @@ db_user = getenv("DB_USER")
 db_password = getenv("DB_PASSWORD")
 db_name = getenv("OLAP_DB")
 
-start_date = datetime.strptime("2021-01-01 00:00:00", "%Y-%m-%d %H:%M:%S") + timedelta(
-    days=-90
-)
-curr_date = start_date
-max_date = datetime.now()
-data = []
-while curr_date <= max_date:
-    data.append(
-        dict(
-            id=int(curr_date.strftime("%Y%m%d%H%M%S")),
-            datetime=curr_date,
-            date=curr_date.date(),
-            month=datetime(year=curr_date.year, month=curr_date.month, day=1),
-            quarter=datetime(
-                year=curr_date.year,
-                month=(math.ceil(curr_date.month / 3) - 1) * 3 + 1,
-                day=1,
-            ).date(),
-            year=curr_date.year,
-        )
-    )
-    curr_date += timedelta(minutes=30)
+end_date = date.today() + timedelta(days=1)
+start_date = end_date - pd.DateOffset(months=15, days=1)
+
+data = pd.DataFrame({"datetime": pd.date_range(start_date, end_date, freq="H")})
+data["id"] = data["datetime"].dt.strftime("%Y%m%d%H%M%S").astype(int)
+data["date"] = data["datetime"].dt.date
+data["month"] = data["datetime"].dt.to_period("M").dt.to_timestamp().dt.date
+data["quarter"] = data["datetime"].dt.to_period("Q").dt.to_timestamp().dt.date
+data["year"] = data["datetime"].dt.year
 
 db_writer = DatabaseWriter(db_user, db_password, db_host, db_port, db_name)
-db_writer.write_dim_date(data)
+db_writer.write_dim_date(data.to_dict(orient="records"))
 db_writer.tear_down()
