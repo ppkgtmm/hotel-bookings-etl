df258248bd2292f1c03431ac8d8f9031cb1cdfea
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-14 00:39:09 +0700

    Update db_writer.py

--- scripts/db_writer.py
+++ scripts/db_writer.py
@@ -1,18 +1,6 @@
 from typing import Any, Dict
 from sqlalchemy import create_engine, text
-
-# , MetaData, Table
 from sqlalchemy.pool import NullPool
-
-# metadata = MetaData()
-
-
-# stg_booking = Table(stg_booking_table, metadata, autoload_with=engine)
-# del_booking = Table(stg_booking_table, metadata, autoload_with=engine)
-# stg_booking_room = Table(stg_booking_table, metadata, autoload_with=engine)
-# del_booking_room = Table(stg_booking_table, metadata, autoload_with=engine)
-# stg_booking_addon = Table(stg_booking_table, metadata, autoload_with=engine)
-# del_booking_addon = Table(stg_booking_table, metadata, autoload_with=engine)
 
 
 def execute_query(connection_string: str, query: str):
