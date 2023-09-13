0f708f7cbcb310d892d38b23819ac4e0fcf129da
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-14 00:31:41 +0700

    Update db_writer.py

--- scripts/db_writer.py
+++ scripts/db_writer.py
@@ -1,10 +1,22 @@
 from typing import Any, Dict
 from sqlalchemy import create_engine, text
+
+# , MetaData, Table
 from sqlalchemy.pool import NullPool
 
+# metadata = MetaData()
 
-def execute_query(connection_string: str, query: str, data: list[Dict[str, Any]]):
+
+# stg_booking = Table(stg_booking_table, metadata, autoload_with=engine)
+# del_booking = Table(stg_booking_table, metadata, autoload_with=engine)
+# stg_booking_room = Table(stg_booking_table, metadata, autoload_with=engine)
+# del_booking_room = Table(stg_booking_table, metadata, autoload_with=engine)
+# stg_booking_addon = Table(stg_booking_table, metadata, autoload_with=engine)
+# del_booking_addon = Table(stg_booking_table, metadata, autoload_with=engine)
+
+
+def execute_query(connection_string: str, query: str):
     engine = create_engine(connection_string, poolclass=NullPool)
     with engine.connect() as conn:
-        conn.execute(text(query), data)
+        conn.execute(text(query))
         conn.commit()
