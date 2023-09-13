c1605760ed8423e79b2dbe55f800b2b0f0dca841
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-13 20:44:27 +0700

    Create dim_guest.py

---
+++ scripts/dimensions/dim_guest.py
@@ -0,0 +1,31 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import expr
+from dotenv import load_dotenv
+from os import getenv
+from common import decode_data, connection_string
+
+load_dotenv()
+
+guests_table = getenv("GUESTS_TABLE")
+dim_guest_table = getenv("DIM_GUEST_TABLE")
+
+
+def process_guests(df: DataFrame, batch_id: int):
+    data: DataFrame = decode_data(df, guests_table)
+
+    processed_data = (
+        data.filter(expr("after IS NOT NULL"))
+        .select("after.*")
+        .withColumnRenamed("id", "_id")
+        .withColumn("created_at", expr("timestamp_millis(updated_at)"))
+        .withColumn("dob", expr("date_from_unix_date(dob)"))
+        .select(["_id", "email", "dob", "gender", "created_at"])
+    )
+    (
+        processed_data.write.format("jdbc")
+        .mode("append")
+        .option("url", connection_string)
+        .option("driver", "com.mysql.jdbc.Driver")
+        .option("dbtable", dim_guest_table)
+        .save()
+    )
