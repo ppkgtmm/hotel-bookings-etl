383f210a24740a4ffcee6f8e88dfa509e067af4b
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-13 20:41:18 +0700

    Create common.py

---
+++ scripts/common.py
@@ -0,0 +1,37 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import expr
+from pyspark.sql.avro.functions import from_avro
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+db_host = getenv("DB_HOST")
+db_port = getenv("DB_PORT")
+db_user = getenv("DB_USER")
+db_password = getenv("DB_PASSWORD")
+db_name = getenv("OLAP_DB")
+
+schema_registry_url = getenv("SCHEMA_REGISTRY_URL")
+
+schema_registry_client = SchemaRegistryClient({"url": schema_registry_url})
+connection_string = "jdbc:mysql://{}:{}@{}:{}/{}?useSSL=false".format(
+    db_user, db_password, db_host, db_port, db_name
+)
+
+
+def get_avro_schema(topic):
+    return schema_registry_client.get_latest_version(topic + "-value").schema.schema_str
+
+
+def to_list(df: DataFrame):
+    return [row.asDict() for row in df.collect()]
+
+
+def decode_data(df: DataFrame, topic: str):
+    return (
+        df.withColumn("data", expr("substring(value, 6, length(value) - 5)"))
+        .withColumn("decoded", from_avro("data", get_avro_schema(topic)))
+        .select("decoded.*")
+    )
