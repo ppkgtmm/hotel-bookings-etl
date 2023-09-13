5de7844612057a5089af0a399445b4f4abeaa1fd
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-13 22:30:41 +0700

    Update db_writer.py

--- scripts/db_writer.py
+++ scripts/db_writer.py
@@ -4,7 +4,7 @@ from sqlalchemy.pool import NullPool
 
 
 def execute_query(connection_string: str, query: str, data: list[Dict[str, Any]]):
-    engine = create_engine(connection_string, pool=NullPool)
+    engine = create_engine(connection_string, poolclass=NullPool)
     with engine.connect() as conn:
         conn.execute(query, data)
         conn.commit()
