a9de32c99e1cc1da79ffac97003de88e94ac599c
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-13 22:36:09 +0700

    Update db_writer.py

--- scripts/db_writer.py
+++ scripts/db_writer.py
@@ -1,10 +1,10 @@
 from typing import Any, Dict
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.pool import NullPool
 
 
 def execute_query(connection_string: str, query: str, data: list[Dict[str, Any]]):
     engine = create_engine(connection_string, poolclass=NullPool)
     with engine.connect() as conn:
-        conn.execute(query, data)
+        conn.execute(text(query), data)
         conn.commit()
