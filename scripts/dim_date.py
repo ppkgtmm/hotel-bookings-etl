d434846682d2aa92e0bec4b9877ef98039a07a15
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-13 17:38:10 +0700

    Update dim_date.py

--- scripts/dim_date.py
+++ scripts/dim_date.py
@@ -1,12 +1,8 @@
-import sys
-from os.path import abspath
 from datetime import date, timedelta
 from dotenv import load_dotenv
 from os import getenv
 import pandas as pd
-
-sys.path.append(abspath("."))
-from utilities.db_writer import DatabaseWriter
+from sqlalchemy import create_engine
 
 load_dotenv()
 
@@ -15,6 +11,11 @@ db_port = getenv("DB_PORT")
 db_user = getenv("DB_USER")
 db_password = getenv("DB_PASSWORD")
 db_name = getenv("OLAP_DB")
+dim_date_table = getenv("DIM_DATE_TABLE")
+
+connection_string = (
+    f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+)
 
 end_date = date.today() + timedelta(days=1)
 start_date = end_date - pd.DateOffset(months=15, days=1)
@@ -26,6 +27,9 @@ data["month"] = data["datetime"].dt.to_period("M").dt.to_timestamp().dt.date
 data["quarter"] = data["datetime"].dt.to_period("Q").dt.to_timestamp().dt.date
 data["year"] = data["datetime"].dt.to_period("Y").dt.to_timestamp().dt.date
 
-db_writer = DatabaseWriter(db_user, db_password, db_host, db_port, db_name)
-db_writer.write_dim_date(data.to_dict(orient="records"))
-db_writer.tear_down()
+engine = create_engine(connection_string)
+
+with engine.connect() as conn:
+    data.to_sql(dim_date_table, conn, index=False, if_exists="append")
+
+engine.dispose()
