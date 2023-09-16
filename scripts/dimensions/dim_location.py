ff6c7a24ee6db64973f567784de362519b9fc505
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-16 17:24:12 +0700

    Create dim_location.py

---
+++ scripts/dimensions/dim_location.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+from os import getenv
+import pandas as pd
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+file_url = getenv("LOCATION_FILE")
+db_host = getenv("DB_HOST")
+db_port = getenv("DB_PORT")
+db_user = getenv("DB_USER")
+db_password = getenv("DB_PASSWORD")
+db_name = getenv("OLAP_DB")
+dim_location_table = getenv("DIM_LOCATION_TABLE")
+
+connection_string = (
+    f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+)
+
+
+data = pd.read_csv(file_url)
+data = data.drop_duplicates(["name", "country_name"])
+data = data.rename({"name": "state", "country_name": "country"})
+
+engine = create_engine(connection_string)
+
+with engine.connect() as conn:
+    data.to_sql(dim_location_table, conn, index=False, if_exists="append")
+
+engine.dispose()
