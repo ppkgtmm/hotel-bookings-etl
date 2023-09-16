409f852c1c8c99b3b134d9c74af950c2b8dfeb7c
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-16 15:35:56 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+CLEAR="\033[0m"
+
+activate() {
+    source venv/bin/activate
+}
+
+down() {
+    docker compose down -v
+}
+
+print() {
+ echo "${GREEN}################################### $1 ###################################${CLEAR}"
+}
+
+setup() {
+    print "running project set up"
+    python3 -m venv venv
+    activate
+    pip3 install -r requirements.txt
+    down
+    docker-compose up -d mysql zookeeper
+	sleep 60
+	docker-compose up -d broker schema-registry kafka-connect --no-recreate
+    print "done setting up project"
+}
+
+datagen() {
+    activate
+    python3 datagen/generate.py
+    print "done generating fake booking data"
+
+}	
+
+seed() {
+    activate
+    dbs/populate.py
+    print "done seeding oltp database"
+}
+
+etl() {
+    print "registering oltp db and running ETL"
+    activate
+    python3 connect/register_source.py
+    python3 scripts/dimensions/dim_date.py
+}
+
+if [ "$1" = "setup" ]
+then
+    setup
+elif [ "$1" = "datagen" ]
+then
+    datagen
+elif [ "$1" = "seed" ]
+then
+    seed
+elif [ "$1" = "etl" ]
+then
+    etl
+elif [ "$1" = "down" ]
+then
+    down   
+else
+    echo "${RED}error : invalid argument${CLEAR}"
+fi
