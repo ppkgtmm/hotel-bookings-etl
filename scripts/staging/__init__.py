303a62df40d6d853ead6619abae6240466eb79a6
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-18 19:39:40 +0700

    Update __init__.py

--- scripts/staging/__init__.py
+++ scripts/staging/__init__.py
@@ -1,4 +1,5 @@
 from .bookings import process_bookings
 from .rooms import process_rooms
 from .booking_rooms import process_booking_rooms
+from .booking_addons import process_booking_addons
 from .guests import process_guests as stage_guest
