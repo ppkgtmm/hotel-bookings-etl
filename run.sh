df0686f50d962de30b29408a73207187ee4a7a4a
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-22 23:17:07 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -48,6 +48,7 @@ etl() {
     python3 connect/register_source.py
     python3 scripts/dimensions/dim_date.py
     python3 scripts/dimensions/dim_location.py
+    docker-compose up -d processor
 }
 
 usage() {  
