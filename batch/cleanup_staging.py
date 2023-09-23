c29a6cd384c685d23c2de1a251476972e798c2d4
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-23 22:57:10 +0700

    Update cleanup_staging.py

--- batch/cleanup_staging.py
+++ batch/cleanup_staging.py
@@ -2,23 +2,21 @@ from airflow import DAG
 from airflow.operators.mysql_operator import MySqlOperator
 from datetime import datetime
 from os import getenv
-from utilities.constants import *
-
+import pytz
 
 mysql_conn_id = getenv("AIRFLOW_OLAP_CONN_ID")
+raw_room_table = getenv("RAW_ROOM_TABLE")
+raw_guest_table = getenv("RAW_GUEST_TABLE")
 
-default_args = dict(
-    owner="airflow",
-    start_date=datetime(2023, 9, 1),
-    depends_on_past=False,
-    schedule_interval="@daily",
-    catchup=False,
-)
+default_args = dict(owner="airflow", depends_on_past=False)
 
 dag = DAG(
-    "olap_stg_cleanup",
+    "cleanup_staging",
     default_args=default_args,
     max_active_runs=1,  # no concurrent runs
+    start_date=datetime(2023, 9, 22, 0, 0, 0, 0, tzinfo=pytz.timezone("Asia/Bangkok")),
+    schedule="30 0 * * *",
+    catchup=False,
 )
 
 cleanup_stg_booking_addons = create_table = MySqlOperator(
@@ -88,18 +86,17 @@ cleanup_del_bookings = MySqlOperator(
 )
 
 cleanup_guests = MySqlOperator(
-    sql=delete_guests_query.format(stg_guest_table=stg_guest_table),
-    task_id="cleanup_guests",
+    sql="scripts/stg_guests.sql",
     mysql_conn_id=mysql_conn_id,
+    params=dict(guests=raw_guest_table),
+    task_id="cleanup_guests",
     dag=dag,
 )
 
 cleanup_rooms = MySqlOperator(
-    sql=delete_rooms_query.format(stg_room_table=stg_room_table),
+    sql="scripts/stg_rooms.sql",
     task_id="cleanup_rooms",
     mysql_conn_id=mysql_conn_id,
+    params=dict(rooms=raw_room_table),
     dag=dag,
 )
-
-[cleanup_stg_booking_rooms, cleanup_del_booking_rooms] >> cleanup_stg_bookings
-[cleanup_stg_booking_rooms, cleanup_del_booking_rooms] >> cleanup_del_bookings
