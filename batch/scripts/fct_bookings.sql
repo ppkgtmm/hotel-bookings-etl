89c8dcd58ce37f955a1eaa4a32b68f92dbcf92bd
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-23 16:22:32 +0700

    Update fct_bookings.sql

--- batch/scripts/fct_bookings.sql
+++ batch/scripts/fct_bookings.sql
@@ -1,8 +1,8 @@
 CREATE TEMPORARY TABLE fact_bookings AS
     WITH RECURSIVE datetimes AS (
         SELECT id booking_id, checkin datetime, checkout
-        FROM {bookings}
-        WHERE is_deleted = false AND (checkin - {date}) < 7
+        FROM {{ params.bookings }}
+        WHERE is_deleted = false AND (checkin - {{ params.date }}) < 7
         UNION ALL
         SELECT booking_id, datetime + INTERVAL 1 DAY, checkout
         FROM datetimes
@@ -14,10 +14,10 @@ CREATE TEMPORARY TABLE fact_bookings AS
             br.booking,
             br.room,
             br.guest
-        FROM {booking_rooms} br
-        INNER JOIN {bookings} b
+        FROM {{ params.booking_rooms }} br
+        INNER JOIN {{ params.bookings }} b
         ON br.booking = b.id
-        WHERE br.is_deleted = false AND br.processed = false AND b.is_deleted = false AND (b.checkin - {date}) < 7
+        WHERE br.is_deleted = false AND br.processed = false AND b.is_deleted = false AND (b.checkin - {{ params.date }}) < 7
     ), bookings AS (
         SELECT
             b.id,
@@ -25,20 +25,20 @@ CREATE TEMPORARY TABLE fact_bookings AS
             b.booking,
             (
                 SELECT MAX(id)
-                FROM {dim_guest}
-                WHERE _id = b.guest AND created_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
+                FROM {{ params.dim_guest }}
+                WHERE _id = b.guest AND created_at <= IF(b.checkin > {{ params.datetime }}, b.checkin, {{ params.datetime }})
             ) guest,
             (
                 SELECT JSON_OBJECT("state", g.state, "country", g.country)
-                FROM {guests} g
-                WHERE g.id = b.guest AND g.updated_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
+                FROM {{ params.guests }} g
+                WHERE g.id = b.guest AND g.updated_at <= IF(b.checkin > {{ params.datetime }}, b.checkin, {{ params.datetime }})
                 ORDER BY g.updated_at DESC
                 LIMIT 1
             ) guest_location,
             (
                 SELECT type
-                FROM {rooms} r
-                WHERE r.id = b.room AND r.updated_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
+                FROM {{ params.rooms }} r
+                WHERE r.id = b.room AND r.updated_at <= IF(b.checkin > {{ params.datetime }}, b.checkin, {{ params.datetime }})
                 ORDER BY r.updated_at DESC
                 LIMIT 1
             ) room_type
@@ -51,13 +51,13 @@ CREATE TEMPORARY TABLE fact_bookings AS
             b.guest,
             (
                 SELECT MAX(id)
-                FROM {dim_location}
+                FROM {{ params.dim_location }}
                 WHERE JSON_EXTRACT(b.guest_location, "$.state") = state AND JSON_EXTRACT(b.guest_location, "$.country") = country
             ) guest_location,
             (
                 SELECT MAX(id)
-                FROM {dim_roomtype}
-                WHERE _id = b.room_type AND created_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
+                FROM {{ params.dim_roomtype }}
+                WHERE _id = b.room_type AND created_at <= IF(b.checkin > {{ params.datetime }}, b.checkin, {{ params.datetime }})
             ) room_type
         FROM bookings b
         WHERE b.guest IS NOT NULL AND b.guest_location IS NOT NULL AND b.room_type IS NOT NULL
@@ -74,11 +74,11 @@ CREATE TEMPORARY TABLE fact_bookings AS
     ON b.booking = dt.booking_id
     WHERE b.guest_location IS NOT NULL AND b.room_type IS NOT NULL;
     
-    INSERT INTO {fct_bookings} (datetime, guest, guest_location, roomtype)
+    INSERT INTO {{ params.fct_bookings }} (datetime, guest, guest_location, roomtype)
     SELECT datetime, guest, guest_location, roomtype
     FROM fact_bookings;
 
-    UPDATE {booking_rooms} br
+    UPDATE {{ params.booking_rooms }} br
     INNER JOIN (
         SELECT id
         FROM fact_bookings
