ac35a9a463b30bbebe2861e94e3cd3e372165deb
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-23 15:13:50 +0700

    Update fct_bookings.py

--- scripts/facts/fct_bookings.py
+++ scripts/facts/fct_bookings.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from os import getenv
-from common import get_connection_string
+from common import get_connection_string, cast_date, cast_datetime
 from db_writer import execute_query
 
 load_dotenv()
@@ -19,7 +19,7 @@ CREATE TEMPORARY TABLE fact_bookings AS
     WITH RECURSIVE datetimes AS (
         SELECT id booking_id, checkin datetime, checkout
         FROM {bookings}
-        WHERE is_deleted = false AND (checkin - CURDATE()) < 7
+        WHERE is_deleted = false AND (checkin - {date}) < 7
         UNION ALL
         SELECT booking_id, datetime + INTERVAL 1 DAY, checkout
         FROM datetimes
@@ -34,7 +34,7 @@ CREATE TEMPORARY TABLE fact_bookings AS
         FROM {booking_rooms} br
         INNER JOIN {bookings} b
         ON br.booking = b.id
-        WHERE br.is_deleted = false AND br.processed = false AND b.is_deleted = false AND (b.checkin - CURDATE()) < 7
+        WHERE br.is_deleted = false AND br.processed = false AND b.is_deleted = false AND (b.checkin - {date}) < 7
     ), bookings AS (
         SELECT
             b.id,
@@ -43,19 +43,19 @@ CREATE TEMPORARY TABLE fact_bookings AS
             (
                 SELECT MAX(id)
                 FROM {dim_guest}
-                WHERE _id = b.guest AND created_at <= IF(b.checkin > CURRENT_TIMESTAMP(), b.checkin, CURRENT_TIMESTAMP())
+                WHERE _id = b.guest AND created_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
             ) guest,
             (
                 SELECT JSON_OBJECT("state", g.state, "country", g.country)
                 FROM {guests} g
-                WHERE g.id = b.guest AND g.updated_at <= IF(b.checkin > CURRENT_TIMESTAMP(), b.checkin, CURRENT_TIMESTAMP())
+                WHERE g.id = b.guest AND g.updated_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
                 ORDER BY g.updated_at DESC
                 LIMIT 1
             ) guest_location,
             (
                 SELECT type
                 FROM {rooms} r
-                WHERE r.id = b.room AND r.updated_at <= IF(b.checkin > CURRENT_TIMESTAMP(), b.checkin, CURRENT_TIMESTAMP())
+                WHERE r.id = b.room AND r.updated_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
                 ORDER BY r.updated_at DESC
                 LIMIT 1
             ) room_type
@@ -74,7 +74,7 @@ CREATE TEMPORARY TABLE fact_bookings AS
             (
                 SELECT MAX(id)
                 FROM {dim_roomtype}
-                WHERE _id = b.room_type AND created_at <= IF(b.checkin > CURRENT_TIMESTAMP(), b.checkin, CURRENT_TIMESTAMP())
+                WHERE _id = b.room_type AND created_at <= IF(b.checkin > {datetime}, b.checkin, {datetime})
             ) room_type
         FROM bookings b
         WHERE b.guest IS NOT NULL AND b.guest_location IS NOT NULL AND b.room_type IS NOT NULL
@@ -106,7 +106,7 @@ CREATE TEMPORARY TABLE fact_bookings AS
 """
 
 
-def process_bookings():
+def process_bookings(datetime: str):
     formatted_query = query.format(
         bookings=raw_booking_table,
         booking_rooms=raw_booking_room_table,
@@ -116,5 +116,7 @@ def process_bookings():
         dim_location=dim_location_table,
         dim_roomtype=dim_roomtype_table,
         fct_bookings=fct_booking_table,
+        date=cast_date(datetime),
+        datetime=cast_datetime(datetime),
     )
     execute_query(get_connection_string(False), formatted_query)
