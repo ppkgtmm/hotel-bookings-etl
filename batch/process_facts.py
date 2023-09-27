285e7f01e33daacc668e629dc9dc501735ca1938
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-28 01:44:08 +0700

    Update process_facts.py

--- batch/process_facts.py
+++ batch/process_facts.py
@@ -1,9 +1,21 @@
 from airflow import DAG
 from airflow.operators.mysql_operator import MySqlOperator
-from airflow.sensors.external_task import ExternalTaskSensor
-from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 import pytz
 from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
+from sqlalchemy.sql import text
+from math import ceil
+
+db_host = getenv("DB_HOST_INTERNAL")
+db_port = getenv("DB_PORT")
+db_user = getenv("DB_USER")
+db_password = getenv("DB_PASSWORD")
+db_name = getenv("OLAP_DB")
+dim_date_table = getenv("DIM_DATE_TABLE")
+fmt = getenv("DT_FORMAT")
 
 mysql_conn_id = getenv("AIRFLOW_OLAP_CONN_ID")
 raw_booking_room_table = getenv("RAW_BOOKING_ROOM_TABLE")
@@ -20,6 +32,45 @@ fct_amenities_table = getenv("FCT_AMENITIES_TABLE")
 
 default_args = dict(owner="airflow", depends_on_past=False)
 
+
+def generate_datetime(base_date: datetime, hour_diff: int):
+    for hour in range(hour_diff):
+        date_time = base_date + timedelta(hours=hour + 1)
+        yield dict(
+            id=int(date_time.strftime(fmt)),
+            datetime=date_time,
+            date=date_time.date(),
+            month=date_time.replace(day=1).date(),
+            quarter=date_time.replace(
+                month=((date_time.month - 1) // 3) * 3 + 1, day=1
+            ).date(),
+            year=date_time.replace(month=1, day=1).date(),
+        )
+
+
+def insert_dim_date(ts: str):
+    engine = create_engine(
+        url=f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}",
+        poolclass=NullPool,
+    )
+    with engine.connect() as conn:
+        max_date = conn.execute(f"SELECT MAX(datetime) FROM {dim_date_table}").all()
+        if not max_date or len(max_date) == 0:
+            return
+        max_date = max_date[0][0]
+        seconds_in_day = 24 * 3600
+        end_date = datetime.fromisoformat(ts) + timedelta(days=8)
+        delta = end_date.replace(tzinfo=None) - max_date
+        hour_diff = ceil((delta.days * seconds_in_day + delta.seconds) / 3600)
+        for data in generate_datetime(max_date, hour_diff):
+            conn.execute(
+                text(
+                    f"INSERT INTO {dim_date_table} VALUES (:id, :datetime, :date, :month, :quarter, :year)"
+                ),
+                data,
+            )
+
+
 dag = DAG(
     "process_facts",
     default_args=default_args,
@@ -29,12 +80,8 @@ dag = DAG(
     schedule="0 7 * * *",
 )
 
-check_dims_populated = ExternalTaskSensor(
-    external_dag_id="process_dims",
-    poke_interval=10,
-    timeout=300,
-    task_id="check_dims_populated",
-    dag=dag,
+ensure_dim_date_populated = PythonOperator(
+    python_callable=insert_dim_date, task_id="ensure_dim_date_populated", dag=dag
 )
 
 process_fct_booking = MySqlOperator(
@@ -72,4 +119,4 @@ process_fct_amenities = MySqlOperator(
     dag=dag,
 )
 
-check_dims_populated >> process_fct_booking >> process_fct_amenities
+ensure_dim_date_populated >> process_fct_booking >> process_fct_amenities
