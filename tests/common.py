56d46d88ec164ebcf8b1123580402bdf8237d1f0
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-27 23:44:22 +0700

    Update common.py

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from os import getenv, path
+import pandas as pd
 
 load_dotenv()
 
@@ -25,3 +26,32 @@ def get_connection_str():
         "oltp": f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{oltp_db}",
         "olap": f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{olap_db}",
     }
+
+
+def get_facts(booking_file: str, booking_room_file: str, booking_addon_file: str):
+    booking = pd.read_csv(booking_file)
+    booking_rooms = pd.read_csv(booking_room_file)
+    booking_addons = pd.read_csv(booking_addon_file)
+
+    booking = booking[["id", "checkin", "checkout"]]
+    booking_addons = booking_addons[
+        ["id", "datetime", "addon", "booking_room", "quantity"]
+    ]
+
+    booking["checkin"] = pd.to_datetime(booking["checkin"])
+    booking["checkout"] = pd.to_datetime(booking["checkout"])
+    booking_addons["datetime"] = pd.to_datetime(booking_addons["datetime"])
+
+    fct_booking = (
+        booking.set_index("id")
+        .join(booking_rooms.set_index("booking"))
+        .rename(columns={"id": "booking_room_id"})
+    )
+
+    fct_amenities = (
+        booking_rooms.set_index("id")
+        .join(booking_addons.set_index("booking_room"))
+        .dropna()
+        .rename(columns={"id": "booking_addon_id"})
+    )
+    return fct_booking, fct_amenities
