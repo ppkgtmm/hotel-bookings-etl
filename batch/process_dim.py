17c257dd4b363ffea461d09b13e759a41da3671d
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-28 01:43:56 +0700

    Update process_dim.py

--- batch/process_dim.py
+++ batch/process_dim.py
@@ -1,58 +1,8 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytz
 from os import getenv
-from sqlalchemy import create_engine
-from sqlalchemy.pool import NullPool
-from sqlalchemy.sql import text
-from math import ceil
-
-db_host = getenv("DB_HOST_INTERNAL")
-db_port = getenv("DB_PORT")
-db_user = getenv("DB_USER")
-db_password = getenv("DB_PASSWORD")
-db_name = getenv("OLAP_DB")
-dim_date_table = getenv("DIM_DATE_TABLE")
-fmt = getenv("DT_FORMAT")
-
-
-def generate_datetime(base_date: datetime, hour_diff: int):
-    for hour in range(hour_diff):
-        date_time = base_date + timedelta(hours=hour + 1)
-        yield dict(
-            id=int(date_time.strftime(fmt)),
-            datetime=date_time,
-            date=date_time.date(),
-            month=date_time.replace(day=1).date(),
-            quarter=date_time.replace(
-                month=((date_time.month - 1) // 3) * 3 + 1, day=1
-            ).date(),
-            year=date_time.replace(month=1, day=1).date(),
-        )
-
-
-def insert_dim_date(ts: str):
-    engine = create_engine(
-        url=f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}",
-        poolclass=NullPool,
-    )
-    with engine.connect() as conn:
-        max_date = conn.execute(f"SELECT MAX(datetime) FROM {dim_date_table}").all()
-        if not max_date or len(max_date) == 0:
-            return
-        max_date = max_date[0][0]
-        seconds_in_day = 24 * 3600
-        end_date = datetime.fromisoformat(ts) + timedelta(days=8)
-        delta = end_date.replace(tzinfo=None) - max_date
-        hour_diff = ceil((delta.days * seconds_in_day + delta.seconds) / 3600)
-        for data in generate_datetime(max_date, hour_diff):
-            conn.execute(
-                text(
-                    f"INSERT INTO {dim_date_table} VALUES (:id, :datetime, :date, :month, :quarter, :year)"
-                ),
-                data,
-            )
 
 
 default_args = dict(owner="airflow", depends_on_past=False)
@@ -64,8 +14,4 @@ dag = DAG(
     catchup=False,
     start_date=datetime(2023, 9, 22, 0, 0, 0, 0, tzinfo=pytz.timezone("Asia/Bangkok")),
     schedule="0 7 * * *",
-)
-
-populate_dim_date = PythonOperator(
-    python_callable=insert_dim_date, task_id="populate_dim_date", dag=dag
 )
