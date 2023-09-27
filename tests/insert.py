3aafbe0ed50dc0c16a869d986c04ce680b0380e9
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-27 23:02:31 +0700

    Update insert.py

--- tests/insert.py
+++ tests/insert.py
@@ -1,57 +1,71 @@
+from typing import List
 from dotenv import load_dotenv
 from os import getenv
 from datetime import datetime, timedelta
 from sqlalchemy import create_engine, Table, MetaData, select, insert
-from common import get_connection_str, get_insert_query
+from common import get_connection_str
+
+booking_id = 100000001
+booking_room_id = [100000001, 100000002]
+booking_addon_id = [100000001, 100000002]
 
 load_dotenv()
 
 # location_table = getenv("LOCATION_TABLE")
-
-# addons_table = getenv("ADDONS_TABLE")
 # roomtypes_table = getenv("ROOMTYPES_TABLE")
+
+addons_table = getenv("ADDONS_TABLE")
 guests_table = getenv("GUESTS_TABLE")
 rooms_table = getenv("ROOMS_TABLE")
 users_table = getenv("USERS_TABLE")
-bookings_table = getenv("BOOKINGS_TABLE")
-booking_rooms_table = getenv("BOOKING_ROOMS_TABLE")
+raw_booking_table = getenv("RAW_BOOKING_TABLE")
+raw_booking_room_table = getenv("RAW_BOOKING_ROOM_TABLE")
+# booking_rooms_table = getenv("BOOKING_ROOMS_TABLE")
 # booking_addons_table = getenv("BOOKING_ADDONS_TABLE")
 
 
+# def insert_booking_addons(booking_rooms: List[int]):
+#     table = Table(booking_addons_table, MetaData(), autoload_with=oltp_engine)
+#     addon_table = Table(addons_table, MetaData(), autoload_with=oltp_engine)
+#     addon_query = select(addon_table.c.id).limit(1)
+#     addon = oltp_conn.execute()
+#     # for booking_room in booking_rooms:
 
-def insert_booking_room(booking: int):
-    table = Table(booking_rooms_table, MetaData(), autoload_with=oltp_engine)
+
+def insert_booking_room(booking: dict):
     room_table = Table(rooms_table, MetaData(), autoload_with=oltp_engine)
     guest_table = Table(guests_table, MetaData(), autoload_with=oltp_engine)
-    room_query = select(room_table.c.id).limit(2)
-    guest_query = select(guest_table.c.id).limit(2)
-    room_ids = oltp_conn.execute(room_query).fetchall()
-    guest_ids = oltp_conn.execute(guest_query).fetchall()
-    ids = []
-    for room, guest in zip(room_ids, guest_ids):
-        data = {"booking": booking, "room": room[0], "guest": guest[0]}
-        query = insert(table).values(**data)
-        result = oltp_conn.execute(query)
-        oltp_conn.commit()
-        ids.append(result.inserted_primary_key)
-    return ids
+    room_ids = oltp_conn.execute(select(room_table.c.id)).fetchmany(2)
+    guest_ids = oltp_conn.execute(select(guest_table.c.id)).fetchmany(2)
+    table = Table(raw_booking_room_table, MetaData(), autoload_with=olap_engine)
+    data = [
+        dict(
+            id=id,
+            booking=booking["id"],
+            room=room[0],
+            guest=guest[0],
+            updated_at=datetime.now(),
+        )
+        for id, room, guest in zip(booking_room_id, room_ids, guest_ids)
+    ]
+    query = insert(table).values(data)
+    olap_conn.execute(query)
+    olap_conn.commit()
+    return data
 
 
 def insert_booking():
-    table = Table(bookings_table, MetaData(), autoload_with=oltp_engine)
-    user_table = Table(users_table, MetaData(), autoload_with=oltp_engine)
-    user_query = select(user_table.c.id).limit(1)
-    user_id = oltp_conn.execute(user_query).fetchone()[0]
     data = dict(
+        id=booking_id,
         checkin=datetime.today() + timedelta(days=10),
         checkout=datetime.today() + timedelta(days=15),
-        payment=datetime.now(),
-        user=user_id,
+        updated_at=datetime.now(),
     )
+    table = Table(raw_booking_table, MetaData(), autoload_with=olap_engine)
     query = insert(table).values(**data)
-    result = oltp_conn.execute(query)
-    oltp_conn.commit()
-    return result.inserted_primary_key
+    olap_conn.execute(query)
+    olap_conn.commit()
+    return data
 
 
 if __name__ == "__main__":
@@ -61,10 +75,10 @@ if __name__ == "__main__":
     oltp_conn = oltp_engine.connect()
     olap_conn = olap_engine.connect()
 
-    booking_id = insert_booking()[0]
-    booking_room_ids = insert_booking_room(booking_id)
-
-    print(booking_id, booking_room_ids)
+    booking = insert_booking()
+    print(booking)
+    booking_rooms = insert_booking_room(booking)
+    print(booking_rooms)
 
     oltp_conn.close()
     olap_conn.close()
