4b570c7136f8b65070117de60462bfca238c82f5
Pinky Gautam <pkgtm1998@gmail.com>
2023-09-27 18:59:25 +0700

    rename functions

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ GREEN="\033[0;32m"
 RED="\033[0;31m"
 CLEAR="\033[0m"
 
-activate() {
+activate_venv() {
     source venv/bin/activate
 }
 
-down() {
+tear_down() {
     docker compose down -v
 }
 
@@ -19,7 +19,7 @@ print() {
 setup() {
     print "running project set up"
     python3.9 -m venv venv
-    activate
+    activate_venv
     pip3 install -r requirements.txt
     down
     docker-compose up -d mysql zookeeper
@@ -29,29 +29,29 @@ setup() {
     print "done setting up project"
 }
 
-datagen() {
-    activate
+generate_data() {
+    activate_venv
     python3 datagen/generate.py
     print "done generating fake booking data"
 
 }	
 
-seed() {
-    activate
+seed_oltp() {
+    activate_venv
     python3 dbs/populate.py
     print "done seeding oltp database"
 }
 
 etl() {
     print "registering oltp db and running ETL"
-    activate
+    activate_venv
     python3 connect/register_source.py
     python3 batch/dependencies/dim_date.py
     python3 batch/dependencies/dim_location.py
     docker-compose up -d stream-processor --no-recreate 
 }
 
-airflow() {
+start_airflow() {
     docker-compose up -d airflow-scheduler airflow-webserver --no-recreate
 }
 
@@ -65,19 +65,19 @@ then
     setup
 elif [ "$1" = "datagen" ]
 then
-    datagen
+    generate_data
 elif [ "$1" = "seed" ]
 then
-    seed
+    seed_oltp
 elif [ "$1" = "etl" ]
 then
     etl
 elif [ "$1" = "airflow" ]
 then
-    airflow
+    start_airflow
 elif [ "$1" = "down" ]
 then
-    down   
+    tear_down
 else
     usage
     echo "${RED}error : invalid argument${CLEAR}"
