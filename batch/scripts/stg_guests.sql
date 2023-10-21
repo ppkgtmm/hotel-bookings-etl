5b2db4b12fcae31f4cef0faf6629a1e00c743d6d
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-21 14:09:42 +0700

    update guests cleanup script

--- batch/scripts/stg_guests.sql
+++ batch/scripts/stg_guests.sql
@@ -1,13 +1,3 @@
-WITH ranked_guests AS (
-    SELECT id, updated_at, ROW_NUMBER() OVER(PARTITION BY id ORDER BY updated_at DESC) rnum
-    FROM {{ params.guests }}
-), to_delete AS (
-    SELECT id, updated_at
-    FROM ranked_guests
-    WHERE rnum > 3
-)
-
-DELETE stg
-FROM {{ params.guests }} stg
-INNER JOIN to_delete tbd
-ON stg.id = tbd.id AND stg.updated_at = tbd.updated_at;
+DELETE
+FROM {{ params.guests }}
+WHERE is_deleted = true
