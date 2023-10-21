2c54ba550abef3c3558704bc8e5416523afc547a
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-21 14:10:00 +0700

    update rooms cleanup script

--- batch/scripts/stg_rooms.sql
+++ batch/scripts/stg_rooms.sql
@@ -1,13 +1,3 @@
-WITH ranked_rooms AS (
-    SELECT id, updated_at, ROW_NUMBER() OVER(PARTITION BY id ORDER BY updated_at DESC) rnum
-    FROM {{ params.rooms }}
-), to_delete AS (
-    SELECT id, updated_at
-    FROM ranked_rooms
-    WHERE rnum > 3
-)
-
-DELETE stg
-FROM {{ params.rooms }} stg
-INNER JOIN to_delete tbd
-ON stg.id = tbd.id AND stg.updated_at = tbd.updated_at;
+DELETE
+FROM {{ params.rooms }}
+WHERE is_deleted = true
