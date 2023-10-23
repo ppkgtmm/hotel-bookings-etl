c123357a1349bd6081334497b3802a9382aa8d9c
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-23 21:35:55 +0700

    Update main.py

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,18 +1,9 @@
 import streamlit as st
-import plotly.express as px
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
-date_format = "DD/MM/YYYY"
-col1, col2 = st.columns(2, gap="medium")
-start_date = col1.date_input(
-    "Start Date", value=datetime.today().replace(day=1), format=date_format
-)
-end_date = col2.date_input(
-    "End Date",
-    value=datetime.today() + relativedelta(months=1, day=1, days=-1),
-    format=date_format,
-)
+date_format = "%Y-%m-%d"
 
-col1, col2 = st.columns(2)
-col1.map()
+st.set_page_config(layout="wide")
+
+col1, col2, col3, col4 = st.columns(4, gap="medium")
+start_date = col2.date_input("start date", value=None)
+end_date = col3.date_input("end date", value=None)
