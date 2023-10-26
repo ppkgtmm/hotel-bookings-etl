15791bdbb6d7d98ddd63f80e0fd3d11fe111fec6
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-26 22:03:44 +0700

    Update mrt_location.sql

--- batch/scripts/mrt_location.sql
+++ batch/scripts/mrt_location.sql
@@ -1,22 +1,22 @@
-insert into {{ params.mrt_location }}
+INSERT INTO {{ params.mrt_location }}
 WITH max_date AS (
-	select MAX(date) max_date 
-	from {{ params.mrt_location }}
+	SELECT MAX(DATE) max_date 
+	FROM {{ params.mrt_location }}
 ), booking_location AS (
 	SELECT
 		d.`date`,
 		l.fips,
 		l.country,
 		l.`state`,
-		sum(t.price) revenue
+		SUM(t.price) revenue
 	FROM {{ params.fct_bookings }} f
-	left join {{ params.dim_date }} d
-	on f.datetime = d.id
+	LEFT JOIN {{ params.dim_date }} d
+	ON f.datetime = d.id
 	LEFT JOIN {{ params.dim_location }} l
 	ON f.guest_location = l.id
 	LEFT JOIN {{ params.dim_roomtype }} t
 	ON f.roomtype = t.id
-	WHERE (SELECT * from max_date) Is null or d.`date` >= (SELECT * from max_date)
+	WHERE (SELECT * FROM max_date) IS NULL OR d.`date` >= (SELECT * FROM max_date)
 	GROUP BY 1, 2, 3, 4
 ), addon_location AS (
 	SELECT
@@ -24,15 +24,15 @@ WITH max_date AS (
 		l.fips,
 		l.country,
 		l.`state`,
-		sum(a.price) revenue
+		SUM(a.price) revenue
 	FROM {{ params.fct_amenities }} f
-	left join {{ params.dim_date }} d
-	on f.datetime = d.id
+	LEFT JOIN {{ params.dim_date }} d
+	ON f.datetime = d.id
 	LEFT JOIN {{ params.dim_location }} l
 	ON f.guest_location = l.id
 	LEFT JOIN {{ params.dim_addon }} a
 	ON f.addon = a.id
-	WHERE (SELECT * from max_date) Is null or d.`date` >= (SELECT * from max_date)
+	WHERE (SELECT * FROM max_date) IS NULL OR d.`date` >= (SELECT * FROM max_date)
 	GROUP BY 1, 2, 3, 4
 )
 SELECT
@@ -45,4 +45,4 @@ COALESCE(a.revenue, 0) addons_revenue,
 b.revenue + COALESCE(a.revenue, 0) total_revenue
 FROM booking_location b
 LEFT JOIN addon_location a
-ON b.date = a.date and b.fips = a.fips;
+ON b.date = a.date AND b.fips = a.fips;
