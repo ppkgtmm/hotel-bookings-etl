10df716c74f50fc9a8f1c649a4fe04526eeaff5e
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-26 21:47:16 +0700

    Create mrt_gender.sql

---
+++ batch/scripts/mrt_gender.sql
@@ -0,0 +1,43 @@
+INSERT INTO {{ params.mrt_gender }}
+WITH max_date AS (
+	SELECT MAX(DATE) max_date 
+	FROM {{ params.mrt_gender }}
+), booking_gender AS (
+	SELECT
+		d.`date`,
+		g.gender,
+		SUM(t.price) revenue
+	FROM fct_bookings f
+	LEFT JOIN dim_date d
+	ON f.datetime = d.id
+	LEFT JOIN dim_guest g
+	ON f.guest = g.id
+	LEFT JOIN dim_roomtype t
+	ON f.roomtype = t.id
+	WHERE (SELECT * FROM max_date) IS NULL OR d.`date` >= (SELECT * FROM max_date)
+	GROUP BY 1, 2
+), addon_gender AS (
+	SELECT 
+		d.`date`,
+		g.gender,
+		SUM(a.price) revenue
+	FROM fct_amenities f
+	LEFT JOIN dim_date d
+	ON f.datetime = d.id
+	LEFT JOIN dim_guest g
+	ON f.guest = g.id
+	LEFT JOIN dim_addon a
+	ON f.addon = a.id
+	WHERE (SELECT * FROM max_date) IS NULL OR d.`date` >= (SELECT * FROM max_date)
+	GROUP BY 1, 2
+)
+
+SELECT
+b.`date`,
+b.gender,
+b.revenue,
+COALESCE(a.revenue, 0) addons_revenue,
+b.revenue + COALESCE(a.revenue, 0) total_revenue
+FROM booking_gender b
+LEFT JOIN addon_gender a
+ON b.date = a.date AND b.gender = a.gender;
