d6df2c9fd6311a69597959870726830846803185
Pinky Gautam <pkgtm1998@gmail.com>
2023-10-27 21:13:21 +0700

    update summary_by_age query

--- dashboard/query.py
+++ dashboard/query.py
@@ -1,32 +1,15 @@
 from sqlalchemy import create_engine, NullPool, text
 
 summary_by_age = """
-WITH cte AS (
-	SELECT 
-		TIMESTAMPDIFF(YEAR, CAST(g.dob AS datetime), CURRENT_TIMESTAMP()) age,
-		t.price
-	FROM fct_bookings b
-	LEFT JOIN dim_guest g
-	ON b.guest = g.id
-	LEFT JOIN dim_roomtype t
-	ON b.roomtype = t.id
-	WHERE b.datetime BETWEEN {start_datetime} AND {end_datetime}
-)
-
-SELECT 
-	CASE
-	WHEN age < 20 THEN 'less than 20'
-	WHEN age BETWEEN 20 AND 29 THEN '20 - 29'
-	WHEN age BETWEEN 30 AND 39 THEN '30 - 39'
-	WHEN age BETWEEN 40 AND 49 THEN '40 - 49'
-	WHEN age BETWEEN 50 AND 59 THEN '50 - 59'
-	ELSE 'at least 60'
-	END age_range,
-	COUNT(1) num_days,
-	SUM(price) revenue
-FROM cte
+SELECT
+	age_range,
+	SUM(revenue) revenue,
+	SUM(addons_revenue) addons_revenue,
+	SUM(total_revenue) total_revenue
+FROM mrt_age
+WHERE date BETWEEN {start_date} AND {end_date}
 GROUP BY 1
-ORDER BY MIN(age);
+ORDER BY MIN(min_age)
 """
 
 summary_by_gender = """
