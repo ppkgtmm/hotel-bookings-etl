b68c57fcabf07a53fa334a7934e61df9062a3aba
Pinky Gautam <pkgtm1998@gmail.com>
2023-11-03 22:31:53 +0700

    remove superset

--- dashboard/run.sh
+++
@@ -1,8 +0,0 @@
-superset fab create-admin --username ${SUPERSET_ADMIN_USERNAME} \
-                --firstname Superset \
-                --lastname Admin \
-                --email admin@superset.com \
-                --password ${SUPERSET_ADMIN_PASSWORD}
-superset db upgrade
-superset init
-/usr/bin/run-server.sh
