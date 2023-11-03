9fec8136296bb8fa104e2dda229843991dfe56cd
Pinky Gautam <pkgtm1998@gmail.com>
2023-11-04 00:15:25 +0700

    Update prepare_data.py

--- dashboard/prepare_data.py
+++ dashboard/prepare_data.py
@@ -5,12 +5,17 @@ import pandas as pd
 
 load_dotenv()
 
+data_file = "full_picture.csv"
+geo_json_file = "states_provinces.json"
 query_file_path = path.abspath(path.join(path.dirname(__file__), "full_picture.sql"))
+output_folder = path.abspath(path.join(path.dirname(__file__), "data"))
+
 db_host = getenv("DB_HOST")
 db_port = getenv("DB_PORT")
 db_user = getenv("DB_USER")
 db_password = getenv("DB_PASSWORD")
 db_name = getenv("DWH_DB")
+geo_json_url = getenv("GEO_JSON_FILE")
 
 connection_str = (
     f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
@@ -22,7 +27,9 @@ if __name__ == "__main__":
 
     with open(query_file_path, "r") as fp:
         query = fp.read()
+
     dashboard_data = pd.read_sql(query, conn)
-    print(dashboard_data)
+    dashboard_data.to_csv(path.join(output_folder, data_file), index=False)
+
     conn.close()
     engine.dispose()
