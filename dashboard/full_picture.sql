6bf470bb0193335436fafcb261d386ae4a035ade
Pinky Gautam <pkgtm1998@gmail.com>
2023-11-04 00:08:02 +0700

    move and rename query file

---
+++ dashboard/full_picture.sql
@@ -0,0 +1,21 @@
+SELECT
+  date,
+  guest,
+  guest_gender,
+  age_range,
+  guest_country,
+  guest_state,
+  fips,
+  room_type,
+  IF(ROW_NUMBER() OVER(PARTITION BY date, guest) = 1, price, 0) price,
+  addons.addon addon_name,
+  addons.quantity addon_quantity,
+  addons.unit_price addon_unit_price
+FROM full_picture
+LEFT JOIN JSON_TABLE(addons, '$[*]' COLUMNS (
+    addon VARCHAR(255) PATH '$.addon',
+    unit_price INTEGER PATH '$.price',
+    quantity INTEGER PATH '$.quantity'
+  )
+) addons
+ON TRUE;
